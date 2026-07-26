df17cb82a5894e66753d82bfd5d2f76f094ce2a5
Hemachandran SS <hemachandran@colanonline.com>
2026-07-26 10:59:36 +0530

    Added Economy Part - 1

---
+++ economy-part-1/indian-economy-topics.js
@@ -0,0 +1,242 @@
+const topics = [
+    {
+        "name": "1_Barter_System__Debtor_and_Creditor__Currency__RBI_and_Banks__Double_coincidence_of_wants__Formal_Sector_Loans__Informal_Sector_Loans__Informal_Sector_Loans__Liquidity__Collateral - EconomyKeyTerminology1.pdf",
+        "pdf": "1_Barter_System__Debtor_and_Creditor__Currency__RBI_and_Banks__Double_coincidence_of_wants__Formal_Sector_Loans__Informal_Sector_Loans__Informal_Sector_Loans__Liquidity__Collateral/EconomyKeyTerminology1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4GgAq0m7yStV4qS50oujK1?si=_qRN8Fu4RB--0x4YmqLZKQ"
+    },
+    {
+        "name": "2_Evolution_of_Modern_Economy - LectureinEconomy-1.pdf",
+        "pdf": "2_Evolution_of_Modern_Economy/LectureinEconomy-1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0rYSwhOebBupd0tdpbamwQ?si=WZlZDxkORMqIm__Suiv2yA"
+    },
+    {
+        "name": "3_Primary_Sector__Secondary_Sector__Tertiary_Sector__Organised_Sector__Unorganised_Sector__Public_Sector__Private_Sector__Public_and_Private_Ltd - EconomyKeyTerminology2.pdf",
+        "pdf": "3_Primary_Sector__Secondary_Sector__Tertiary_Sector__Organised_Sector__Unorganised_Sector__Public_Sector__Private_Sector__Public_and_Private_Ltd/EconomyKeyTerminology2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3s1xh2rOxX5Pl31L65X807?si=NpA0V_VXSDagBvLkafxsDQ"
+    },
+    {
+        "name": "4_Evolution_of_Various_Sectors_of_Economy - LectureinEconomy-2.pdf",
+        "pdf": "4_Evolution_of_Various_Sectors_of_Economy/LectureinEconomy-2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3YqiMKuS6KuyeIW3gCPDJN?si=3EbSTqc8RLWkkMpUaeDe_w"
+    },
+    {
+        "name": "5_Green_Revolution__HYV_seeds__Famine__Fair_Price_Shops__Buffer_Stocks__Poverty__Poverty_Line__Casual_Wage_Labourer - EconomyKeyTerminology3.pdf",
+        "pdf": "5_Green_Revolution__HYV_seeds__Famine__Fair_Price_Shops__Buffer_Stocks__Poverty__Poverty_Line__Casual_Wage_Labourer/EconomyKeyTerminology3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/02yg4P8HpHvFp85qGSYtK2?si=lQ5tdBVQQVyIP-JZBIo_qw"
+    },
+    {
+        "name": "6_Poverty_and_Food_Security - LectureinEconomy-3.pdf",
+        "pdf": "6_Poverty_and_Food_Security/LectureinEconomy-3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/28ZhLvOCT6YOukOr5uwfMu?si=3Zfcwvp9Qgaz2ZTw8fiKbA"
+    },
+    {
+        "name": "7_Sustainable_Developement__Renewable_Resources__Renewable_Energy__Literacy_Rate__Net_Attendance_Ratio__Physical_Capital__Human_Capital__Productivity - EconomyKeyTerminology4.pdf",
+        "pdf": "7_Sustainable_Developement__Renewable_Resources__Renewable_Energy__Literacy_Rate__Net_Attendance_Ratio__Physical_Capital__Human_Capital__Productivity/EconomyKeyTerminology4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3XONP9PjWumZ4yhfLiypWI?si=fP3uLMTURI27Y-PtgnABzA"
+    },
+    {
+        "name": "8_Physical_Capital_&_Human_Capital - LectureinEconomy-4.pdf",
+        "pdf": "8_Physical_Capital_&_Human_Capital/LectureinEconomy-4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3wu8UOmdx1Unmpjz46CF2P?si=7FU_QPq9R7Gd0OTDuCYFXw"
+    },
+    {
+        "name": "9_Goods_&_Services__Sectors_of_Economy__Trade__Exports_&_Imports__Investments__Shares__Inflation__FPI_&_FDI__MNC's_&_Globalisation - EconomyKeyTerminology5.pdf",
+        "pdf": "9_Goods_&_Services__Sectors_of_Economy__Trade__Exports_&_Imports__Investments__Shares__Inflation__FPI_&_FDI__MNC's_&_Globalisation/EconomyKeyTerminology5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3zVmzKyfYgjpFNGwpeXw5i?si=IGDv4WlcTBWIvykUqFvGhQ"
+    },
+    {
+        "name": "10_Globalisation-part1 - LectureinEconomy-5.pdf",
+        "pdf": "10_Globalisation-part1/LectureinEconomy-5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3zVmzKyfYgjpFNGwpeXw5i?si=IGDv4WlcTBWIvykUqFvGhQ"
+    },
+    {
+        "name": "11_Inflation__FPI_&_FDI__Exchange_Rates__Multinational_Corporations__Appreciation_&_Depreciation__Globalisation__Liberalisation__Privatisation - EconomyKeyTerminology6.pdf",
+        "pdf": "11_Inflation__FPI_&_FDI__Exchange_Rates__Multinational_Corporations__Appreciation_&_Depreciation__Globalisation__Liberalisation__Privatisation/EconomyKeyTerminology6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5k3J0eRU7Y6hhjIbAnMAUA?si=1wUIK3KCSsyHrXPxOuLeTA"
+    },
+    {
+        "name": "12_Globalisation-part2 - LectureinEconomy-6.pdf",
+        "pdf": "12_Globalisation-part2/LectureinEconomy-6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5gWHdQwWuEwBoDqU41beMw?si=RU9pqWUOSNCEC4L6khBizQ"
+    },
+    {
+        "name": "13_GDP_&_GNP__NDP_&_NNP__Productivity__Infant_Mortality_Rate__Life_Expectancy_at_Birth__Morbidity__Land_Settlement__Suez_Canal__Colonial_Rule - ECONOMYTERMINOLOGYFORWEB-007-1493453393.pdf",
+        "pdf": "13_GDP_&_GNP__NDP_&_NNP__Productivity__Infant_Mortality_Rate__Life_Expectancy_at_Birth__Morbidity__Land_Settlement__Suez_Canal__Colonial_Rule/ECONOMYTERMINOLOGYFORWEB-007-1493453393.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3qtjONSTOgKXKwa03D78S2?si=NBp4c4lkRhWWQ480sbI0gg"
+    },
+    {
+        "name": "14_Indian_economy-_State_of_Various_Sectors_at_the_time_of_Independence - ECONOMYLECTURESFORWEBMODULE-007-1493453567.pdf",
+        "pdf": "14_Indian_economy-_State_of_Various_Sectors_at_the_time_of_Independence/ECONOMYLECTURESFORWEBMODULE-007-1493453567.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5EmcssRKaFBuBeI82dB73p?si=SuZqHtFATXOLh1E4w3HigQ"
+    },
+    {
+        "name": "15_Capitalist_Economy__Socialist_Economy__Mixed_Economy__Import_Licensing_&_Substitution_ - EconomyTerminology-008-1493467302.pdf",
+        "pdf": "15_Capitalist_Economy__Socialist_Economy__Mixed_Economy__Import_Licensing_&_Substitution_/EconomyTerminology-008-1493467302.pdf",
+        "spotify_link": "https://open.spotify.com/episode/31a8wICHpraWe5mEcntFa8?si=BgnVi1XgTTGMBjAAPufzVA"
+    },
+    {
+        "name": "16_Indian_Economy_1947_to_1990_Part-1 - EconomyLecture-008-1494258912.pdf",
+        "pdf": "16_Indian_Economy_1947_to_1990_Part-1/EconomyLecture-008-1494258912.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5ztMAPSXgiOBXvM45PZOmI?si=DA4DXKYZTa-xSbUOH2Jzig"
+    },
+    {
+        "name": "17_Permit_License_Raj__Small_Scale_Industry__MSME__Import_Substitution__Import_Tariffs__Import_Quotas__Occupation_Structure_of_Economy - EconomyTerminology-009-1494254615.pdf",
+        "pdf": "17_Permit_License_Raj__Small_Scale_Industry__MSME__Import_Substitution__Import_Tariffs__Import_Quotas__Occupation_Structure_of_Economy/EconomyTerminology-009-1494254615.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1dfELOxWdRlBBMMJlgqqyz?si=Yj3cmfEnSninyOlfAEoj_A"
+    },
+    {
+        "name": "18_Indian_Economy_1947_to_1990_Part-2 - EconomyLecture-009-1494256374.pdf",
+        "pdf": "18_Indian_Economy_1947_to_1990_Part-2/EconomyLecture-009-1494256374.pdf",
+        "spotify_link": "https://open.spotify.com/episode/53sQTp6tXih48TtbaWZEFD?si=-l5yGE_2T7SYBTnsuO0uYQ"
+    },
+    {
+        "name": "19_Globlisation__Liberalisation__Privatisation__Disinvestments__World_Bank_&_IMF__Financial__institutions__Stock_Exchanges__Exim_Policy__WTO__Structural_Reforms - EconomyTerminology-010-1494591070.pdf",
+        "pdf": "19_Globlisation__Liberalisation__Privatisation__Disinvestments__World_Bank_&_IMF__Financial__institutions__Stock_Exchanges__Exim_Policy__WTO__Structural_Reforms/EconomyTerminology-010-1494591070.pdf",
+        "spotify_link": "https://open.spotify.com/episode/172ohHjnVVBjXHvodzua7b?si=x4uoZFsESi69G8DzMsAaOQ"
+    },
+    {
+        "name": "20_Liberalisation_&_privatisation - EconomyLecture-010-1494600042.pdf",
+        "pdf": "20_Liberalisation_&_privatisation/EconomyLecture-010-1494600042.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1pukSPc3tglCzc31Fa1v00?si=61LwiDMvRQ2sgggoicufxw"
+    },
+    {
+        "name": "21_Balance_of_Payments__Trade_Deficit__Current_Account__Capital_Account__Nationalism__Nativism__Protectionism__Emigrant_&_Immigrant - EconomyTerminology-011pdf-1494681412.pdf",
+        "pdf": "21_Balance_of_Payments__Trade_Deficit__Current_Account__Capital_Account__Nationalism__Nativism__Protectionism__Emigrant_&_Immigrant/EconomyTerminology-011pdf-1494681412.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6V1tQ8KNhlzBdIibgNua2y?si=z3jhxm0BRcGkpARv8Q9LOg"
+    },
+    {
+        "name": "22_Globalisation - EconomyLecture-011pdf-1494857191.pdf",
+        "pdf": "22_Globalisation/EconomyLecture-011pdf-1494857191.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7INEtzPrMjtkGegDortVj7?si=52hc6CXYSWC3QwO9G0ByCw"
+    },
+    {
+        "name": "23_Social_security__Poverty__Poverty_Line__CSO__NSSO__Nutrition__Malnutrition__Under_Nutrition__Hidden_Hunger__Micronutrients - EconomyTerminology-012-1495268144.pdf",
+        "pdf": "23_Social_security__Poverty__Poverty_Line__CSO__NSSO__Nutrition__Malnutrition__Under_Nutrition__Hidden_Hunger__Micronutrients/EconomyTerminology-012-1495268144.pdf",
+        "spotify_link": "https://open.spotify.com/episode/50GkMbjbWbI7v528JtH7N4?si=rOpTZaCgTSGZIBs6pKs4Yw"
+    },
+    {
+        "name": "24_Poverty - EconomyLecture-012-1495287681.pdf",
+        "pdf": "24_Poverty/EconomyLecture-012-1495287681.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "25_Organised_-_Unorganised__Unemployment__Underemployement__Disguised_Unemployment__Seasonal_Unemployment__Cyclic_Unemployment__Structural_Unemployment__Technology_Unemployment.pdf",
+        "pdf": "25_Organised_/_Unorganised__Unemployment__Underemployement__Disguised_Unemployment__Seasonal_Unemployment__Cyclic_Unemployment__Structural_Unemployment__Technology_Unemployment/EconomyTerminology-013-1495460775.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6Ha245NO8VCFOcv8dqdXWu?si=_Jk9ZK6WRVa93PJQZYquIg"
+    },
+    {
+        "name": "26_Unemployment - EconomyLecture-013-1495461570.pdf",
+        "pdf": "26_Unemployment/EconomyLecture-013-1495461570.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6tUz2AJ6NW67uUCqisrouj?si=pzEGjnwxTb2yIk0W3WmUTQ"
+    },
+    {
+        "name": "27_Haircut__Protectionism__Bank_Run__Retrospective_Tax__Business_Cycle__Recession_ - ",
+        "pdf": "27_Haircut__Protectionism__Bank_Run__Retrospective_Tax__Business_Cycle__Recession_/",
+        "spotify_link": "https://open.spotify.com/episode/6QZFyuBBtESiGyS80RNL7x?si=2-aS1_POTUqIA8nELJPJ8A"
+    },
+    {
+        "name": "28_Infrastructure - ECONOMYLETURESFORWEBMODULE-014-1495887081.pdf",
+        "pdf": "28_Infrastructure/ECONOMYLETURESFORWEBMODULE-014-1495887081.pdf",
+        "spotify_link": "https://open.spotify.com/episode/42oaFW5U2eZuKZ4q3NF3yD?si=EXNh3ZYqRdWtRZhQkEJFfg"
+    },
+    {
+        "name": "29_Inflation_WPI_Inflation_CPI_Inflation_Types_of_Inflation__Fiscal_Policy__Monetary_Policy - EconomyTerminology-015-1495887740.pdf",
+        "pdf": "29_Inflation_WPI_Inflation_CPI_Inflation_Types_of_Inflation__Fiscal_Policy__Monetary_Policy/EconomyTerminology-015-1495887740.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4eA1zsTWnjla9VwrTlgwH0?si=eGb-tO6QSnuBn_hJRFCYqg"
+    },
+    {
+        "name": "30_Inflation - ECONOMYLETURESFORWEBMODULE-015-1495889518.pdf",
+        "pdf": "30_Inflation/ECONOMYLETURESFORWEBMODULE-015-1495889518.pdf",
+        "spotify_link": "https://open.spotify.com/episode/258wFiunXTjDdHWXtLnkGG?si=3V4Ld9hbRciegrvuEjTDqg"
+    },
+    {
+        "name": "31_Money__Demand_for_Money__Currency__Legal_Tender__Monetary_Base__Fiat_Money__Money_Supply__Coins__Notes__CDR_&_RDR - EconomyTerminology-016-1496927698.pdf",
+        "pdf": "31_Money__Demand_for_Money__Currency__Legal_Tender__Monetary_Base__Fiat_Money__Money_Supply__Coins__Notes__CDR_&_RDR/EconomyTerminology-016-1496927698.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2r8l1def7kFcm6xVyzFkZM?si=wuEGNGDGQMi_9vDIzYtwSw"
+    },
+    {
+        "name": "32_Indian_Banking_System - EconomyLecture-016-1496927881.pdf",
+        "pdf": "32_Indian_Banking_System/EconomyLecture-016-1496927881.pdf",
+        "spotify_link": "https://open.spotify.com/episode/119WBTdwW62NYOVFnuPKtF?si=ihBsdXs9StWsEHI3AKqLfw"
+    },
+    {
+        "name": "33_Demonitisation__Money_Supply__Narrow_Money__Broad_Money__Letter_of_Credit__Ombudsman__DTAA__POS_Terminals__Bank_Gaurentee__LIBOR_ - EconomyTerminology-017-1497001717.pdf",
+        "pdf": "33_Demonitisation__Money_Supply__Narrow_Money__Broad_Money__Letter_of_Credit__Ombudsman__DTAA__POS_Terminals__Bank_Gaurentee__LIBOR_/EconomyTerminology-017-1497001717.pdf",
+        "spotify_link": "https://open.spotify.com/episode/481epsOt7RnDJ6sJXeWaVU?si=oJItqU8aTYeRY0S1p9zxgw"
+    },
+    {
+        "name": "34_Banking_System_Intricacies - EconomyLecture-017-1497007293.pdf",
+        "pdf": "34_Banking_System_Intricacies/EconomyLecture-017-1497007293.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4OzFJejK7zF2mgkahz9DDr?si=9iZBb0JvRdi0yqI2LShLsA"
+    },
+    {
+        "name": "35_NPCI__Rupay_Card__Overdraft__Universal_Banks__Niche_Banks__Term_Loans__Soft_Loans__Secured_Loan__Insolvency__Bankruptcy - EconomyTerminology-018-1497010879.pdf",
+        "pdf": "35_NPCI__Rupay_Card__Overdraft__Universal_Banks__Niche_Banks__Term_Loans__Soft_Loans__Secured_Loan__Insolvency__Bankruptcy/EconomyTerminology-018-1497010879.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6frSROydrvFly6N7hCK6rT?si=KjbPXDYXSnWgih4YaGabgg"
+    },
+    {
+        "name": "36_RBI_&_Monetary_Policy - ECONOMYLecture-18-1497016568.pdf",
+        "pdf": "36_RBI_&_Monetary_Policy/ECONOMYLecture-18-1497016568.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4EsgEvcVkRpoiVdIbhuAo5?si=Wd0dEM6ISK6rQoMXj3RdHw"
+    },
+    {
+        "name": "37_Cooperative_Banks__Regional_Rural_Banks__NABARD__Depositories__Primary_Dealers__Authorised_Dealers__Developed_Countries__Developing_Countries__Consortium_Loans__JLF - EconomyTerminology-019-1497428730.pdf",
+        "pdf": "37_Cooperative_Banks__Regional_Rural_Banks__NABARD__Depositories__Primary_Dealers__Authorised_Dealers__Developed_Countries__Developing_Countries__Consortium_Loans__JLF/EconomyTerminology-019-1497428730.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4ibU51bNfJE7qD7YYXUtT0?si=J2FRV8GCQbKLBvLZ9ffKtA"
+    },
+    {
+        "name": "38_Financial_Market - EconomyLC-019-1497438344.pdf",
+        "pdf": "38_Financial_Market/EconomyLC-019-1497438344.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3fB2aeydVi7zGJKniSbIRB?si=7qE0hRx9R86fn_ha47sJRQ"
+    },
+    {
+        "name": "39_Final_Goods__Intermediate_Goods__Consumer_Goods__Capital_Goods__Stock_&_Flow__Gross_Capital_Formation__Depreciation__Inventory__Factors_of_Production__Circular_Flow_of_Income - EconomyTerminology-020-1497608498.pdf",
+        "pdf": "39_Final_Goods__Intermediate_Goods__Consumer_Goods__Capital_Goods__Stock_&_Flow__Gross_Capital_Formation__Depreciation__Inventory__Factors_of_Production__Circular_Flow_of_Income/EconomyTerminology-020-1497608498.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5nMocSu8vk9gE2rcomTgBb?si=qcCkummARaWzooBGrVllGw"
+    },
+    {
+        "name": "40_Concepts_of_GDP_Calculation - ECONOMYLecture-20-1497608656.pdf",
+        "pdf": "40_Concepts_of_GDP_Calculation/ECONOMYLecture-20-1497608656.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4ARTyAw9aRlMocCDBrshRM?si=16jchdj1RDegq4HnLyBM6w"
+    },
+    {
+        "name": "41_GDP__GNP__NDP__NNP__GDP_at_Constant_Prices__GDP_at_Current_Prices__GDP_Deflator_&_CPI__GDP_Data_&_8_Sectors - EconomyTerminology-021-1497694132.pdf",
+        "pdf": "41_GDP__GNP__NDP__NNP__GDP_at_Constant_Prices__GDP_at_Current_Prices__GDP_Deflator_&_CPI__GDP_Data_&_8_Sectors/EconomyTerminology-021-1497694132.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2gesRC3pQtz2e2HT1ycoHg?si=Rg5qPLddQs699lW6VrmfOg"
+    },
+    {
+        "name": "42_GDP_and_Intricacies - EconomyLecture-21-1497698891.pdf",
+        "pdf": "42_GDP_and_Intricacies/EconomyLecture-21-1497698891.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0rR5DIIBVyBJnOChlQRwVK?si=aEeX-u3hS6a34S0vnhXl9Q"
+    },
+    {
+        "name": "43_Public_Goods__Private_Goods__Revenue_Receipts__Capital_Receipts__Revenue_Expenditure__Capital_Expenditure__Allocation_Function__Distribution_Function__Stabilization_Fuction - EconomyTerminology-022-1498193908.pdf",
+        "pdf": "43_Public_Goods__Private_Goods__Revenue_Receipts__Capital_Receipts__Revenue_Expenditure__Capital_Expenditure__Allocation_Function__Distribution_Function__Stabilization_Fuction/EconomyTerminology-022-1498193908.pdf",
+        "spotify_link": "https://open.spotify.com/episode/50XYp1zyMLboJ3XOSlpchM?si=3LIK-3vlT-6Pfwkfi-lbDQ"
+    },
+    {
+        "name": "44_Receipts_&_Expenditure_of_the_Govt - EconomyLecture-22-1498194094.pdf",
+        "pdf": "44_Receipts_&_Expenditure_of_the_Govt/EconomyLecture-22-1498194094.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5pBBVCwuOVP2w1nKS2Zyr7?si=6c3RlY1oTX-bPrezZ2ORNQ"
+    },
+    {
+        "name": "45_Annual_Financial_Statement__Consolidated_Fund_of_India__Contingency_Fund_of_India__Public_Account__Finance_Bill__Demands_for_Grants__Appropriation_Bill__Charged_Expenditure - EconomyKeyTerminology-23-1498463236.pdf",
+        "pdf": "45_Annual_Financial_Statement__Consolidated_Fund_of_India__Contingency_Fund_of_India__Public_Account__Finance_Bill__Demands_for_Grants__Appropriation_Bill__Charged_Expenditure/EconomyKeyTerminology-23-1498463236.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2eugCBPjzScbjkkien9wQM?si=hgHIssO_QnG4eCmeEl1HNg"
+    },
+    {
+        "name": "46_Union_Budget - LectureinEconomy-23-1498463400.pdf",
+        "pdf": "46_Union_Budget/LectureinEconomy-23-1498463400.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0CMz2aB55nO3pMWEdSAtma?si=_VhOIu0LQpyMXHBbJ-RxTA"
+    },
+    {
+        "name": "47_Economic_Policy__Fiscal_Deficit__Revenue_Deficit__Primary_Deficit__Public_Debt__Fiscal_Policy__Types_of_Fiscal_Policy__Fiscal_Stimulus - EconomyKeyTeminology-24-1498622769.pdf",
+        "pdf": "47_Economic_Policy__Fiscal_Deficit__Revenue_Deficit__Primary_Deficit__Public_Debt__Fiscal_Policy__Types_of_Fiscal_Policy__Fiscal_Stimulus/EconomyKeyTeminology-24-1498622769.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0VirphCDwxk37MeaUmCexg?si=D7epIATxSBCtHFmCQSdMyQ"
+    },
+    {
+        "name": "48_Fiscal_Policy - LectureinEconomy-24-1498623000.pdf",
+        "pdf": "48_Fiscal_Policy/LectureinEconomy-24-1498623000.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4SyFl5dnleT60oULBtQbW7?si=9bdkq1RZRQiFy4YXBDkL9w"
+    }
+];
