16e5eccfb07f7c39e1d991531913fa495d3896b9
Hemachandran SS <hemachandran@colanonline.com>
2026-07-27 22:59:23 +0530

    Added Ancient History

---
+++ ancient-history/ancient-history-topic.js
@@ -0,0 +1,117 @@
+const topics = [
+    {
+        "name": "1_Introduction/AncientHistory-Introduction-1544539684.pdf",
+        "pdf": "1_Introduction/AncientHistory-Introduction-1544539684.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4bBCIkoOd2BAAEai00D4ej?si=On1gv9SyRaGjR4YVPJBmfQ"
+    },
+    {
+        "name": "2_pre-history-part-1/AncientHistory-Pre-History-Part1-1544708901.pdf",
+        "pdf": "2_pre-history-part-1/AncientHistory-Pre-History-Part1-1544708901.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5FlIiWntTMN1nGVe5MofW2?si=2rwpN35pRDGJoTGldp2xDg"
+    },
+    {
+        "name": "3_Pre-History-Part2/AncientHistory-Pre-History-Part2-1544796098.pdf",
+        "pdf": "3_Pre-History-Part2/AncientHistory-Pre-History-Part2-1544796098.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0tXZ4hweAFzotqZ1hvN4Q1?si=B_in80UFSC2tr9KO156jJg"
+    },
+    {
+        "name": "4_Pre-History_MesoLithic_ERA/AncientHistory-Module4-1545058562.pdf",
+        "pdf": "4_Pre-History_MesoLithic_ERA/AncientHistory-Module4-1545058562.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0Y8yhUcc7FEGKaNb3yRZdv?si=iGDX7tbhSfWtLH1_TY1t1g"
+    },
+    {
+        "name": "5_Pre-History_Neolithic_ERA/AncientHistory-Module5-1545141322.pdf",
+        "pdf": "5_Pre-History_Neolithic_ERA/AncientHistory-Module5-1545141322.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5vnUVM38iPdKjFrVB1Ypsc?si=BPdjwl2hTXmOHlxos3p73g"
+    },
+    {
+        "name": "6_Pre-History_Metal_Age/AncientHistory-Module6-1545230827.pdf",
+        "pdf": "6_Pre-History_Metal_Age/AncientHistory-Module6-1545230827.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5Ea4F3u7QZwb75Yp0jebnr?si=owsnx1ZOQ5ioZGtZFx0-xg"
+    },
+    {
+        "name": "7_Proto-History_Indus_Valley_Civilization-1/AncientHistory-Module7-1545401994.pdf",
+        "pdf": "7_Proto-History_Indus_Valley_Civilization-1/AncientHistory-Module7-1545401994.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4LIptHFHD6kFqHWI0PavLZ?si=fFK3h0xdSqWxTvM4bvMOUQ"
+    },
+    {
+        "name": "8_Proto-History_Indus_Valley_Civilization-2/AncientHistory-Module8-1545482228.pdf",
+        "pdf": "8_Proto-History_Indus_Valley_Civilization-2/AncientHistory-Module8-1545482228.pdf",
+        "spotify_link": "https://open.spotify.com/episode/597DPmf0epQY2UGXUjAVZy?si=gO_tdFFlRn638ICjTypRkA"
+    },
+    {
+        "name": "9_Vedic_Civilization_part-1/VedicCivilization-Part1-1545837083.pdf",
+        "pdf": "9_Vedic_Civilization_part-1/VedicCivilization-Part1-1545837083.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1XNb3AiiEKQxSxI5tE3o8i?si=DGFZl4BlSNCiRhMFOwoxyA"
+    },
+    {
+        "name": "10_Vedic_Civilization_part-2/VedicCivilization-Part2-1545837020.pdf",
+        "pdf": "10_Vedic_Civilization_part-2/VedicCivilization-Part2-1545837020.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6mdikmJ7NI6EFArkQzKQ4F?si=K3i0sa1wRwOOhG9fGa6CWQ"
+    },
+    {
+        "name": "11_Mahajanapadas_part-1/AncientHistory-Mahajanapadas-1546006212.pdf",
+        "pdf": "11_Mahajanapadas_part-1/AncientHistory-Mahajanapadas-1546006212.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6A5NJ7gu1QRXEnRFzugIxi?si=UWkPy0r6RNayfwQx8HWCwQ"
+    },
+    {
+        "name": "12_Mahajanapadas_part-2/AncientHistory-Mahajanapadas-Part2-1546081876.pdf",
+        "pdf": "12_Mahajanapadas_part-2/AncientHistory-Mahajanapadas-Part2-1546081876.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3IPyjRpA5ODgN5aeRnmuRw?si=DEMxJjxARqC8AqX00dOARw"
+    },
+    {
+        "name": "13_MauryanDynasty-Part1/AncientHistory-MauryanDynasty-Part1-1546261308.pdf",
+        "pdf": "13_MauryanDynasty-Part1/AncientHistory-MauryanDynasty-Part1-1546261308.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7D9puMNNN7b6M2tIOqxS3J?si=beLGURFeRn6bsEHFBSMu0w"
+    },
+    {
+        "name": "14_MauryanDynasty-Part2/AncientHistory-MauryanDynasty-Part2-1546342325.pdf",
+        "pdf": "14_MauryanDynasty-Part2/AncientHistory-MauryanDynasty-Part2-1546342325.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1gut5FxcAi5uBGPzdVYySN?si=WN97i3yAQ0SFSRUxq70rCQ"
+    },
+    {
+        "name": "15_PostMauryan-NorthernEmpires/AncientHistory-PostMauryan-NorthernEmpires-1546431219.pdf",
+        "pdf": "15_PostMauryan-NorthernEmpires/AncientHistory-PostMauryan-NorthernEmpires-1546431219.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5nh4TvKnbayXhfU3ULwdub?si=LJaWL0d-T1Want5_yLxXTw"
+    },
+    {
+        "name": "16_PostMauryan-NorthWestEmpires/AncientHistory-PostMauryan-NorthWestEmpires-1546524036.pdf",
+        "pdf": "16_PostMauryan-NorthWestEmpires/AncientHistory-PostMauryan-NorthWestEmpires-1546524036.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5nfHdVpX0dbfovbLKjGSlQ?si=vii815JTQrqnKdhvWAeExA"
+    },
+    {
+        "name": "17_Satavahana_Dynasty/AncientHistory-SatavahanaDynasty-1546669303.pdf",
+        "pdf": "17_Satavahana_Dynasty/AncientHistory-SatavahanaDynasty-1546669303.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0Unplzqcx6fKNJX1i62nvn?si=wmzYnCliRg-XZ0Hz_FflMg"
+    },
+    {
+        "name": "18_Gupta_Dynasty_part_-1/AncientHistory-GuptaDynasty-Part1-1546870468.pdf",
+        "pdf": "18_Gupta_Dynasty_part_-1/AncientHistory-GuptaDynasty-Part1-1546870468.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6YIFi3aXTtTAwCcgC93YNx?si=-V0lJmbKTDO6YMX5OXl7Jg"
+    },
+    {
+        "name": "19_Gupta_Dynasty_part_-2/AncientHistory-GuptaDynasty-Part2-1547041916.pdf",
+        "pdf": "19_Gupta_Dynasty_part_-2/AncientHistory-GuptaDynasty-Part2-1547041916.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4j6r7pzEwGeT3sQxbsfqWI?si=RRt8j13JSOCshGutIl90Pg"
+    },
+    {
+        "name": "20_Gupta_Dynasty_part_-3/AncientHistory-GuptaDynasty-Part3-1547121782.pdf",
+        "pdf": "20_Gupta_Dynasty_part_-3/AncientHistory-GuptaDynasty-Part3-1547121782.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5abcUr4BiE444iQqc3LZLS?si=OBHz1PJoSFy661oL_1HHgQ"
+    },
+    {
+        "name": "21_Pushyabhuti_Dynasti_part-1/AncientHistory-Module21-1552627261.pdf",
+        "pdf": "21_Pushyabhuti_Dynasti_part-1/AncientHistory-Module21-1552627261.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4UlxRWyZBf4EgMEAwBDsa7?si=PCD0NhfLQTCfv_zFxBQEvA"
+    },
+    {
+        "name": "22_PushyabhutiDynasty-Part2/AncientHistory-PushyabhutiDynasty-Part2-1547214181.pdf",
+        "pdf": "22_PushyabhutiDynasty-Part2/AncientHistory-PushyabhutiDynasty-Part2-1547214181.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5tHsP0I917Pj8KQGGDAUPy?si=iMDWSHvhQYGLmuVV3r_7hw"
+    },
+    {
+        "name": "23_SANGAM_AGE/Ancienthistory-23-1547279310.pdf",
+        "pdf": "23_SANGAM_AGE/Ancienthistory-23-1547279310.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1RjpO7Cl8isHo1UX1m5l8y?si=MWI4ej2sTjiSZKZxGv9-dQ"
+    }
+];
