e210be3a0d6bafe61c5108b44f6a8911fb67799f
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 09:26:36 +0530

    Added Modern India Movements

---
+++ modern-india-movements/modern-india-movements-topic.js
@@ -0,0 +1,62 @@
+const topics = [
+    {
+        "name": "1_Socio-Religious_Reform_Movements_Part-1",
+        "pdf": "1_Socio-Religious_Reform_Movements_Part-1/ModernIndianHistory-Movements-Module1-1555145437.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6DK2TGJfLxJCnzKn3Jr3oS?si=cX9LYzTKQtOHiISIjA0rsg"
+    },
+    {
+        "name": "2_Socio-Religious_Reform_Movements_Part-2",
+        "pdf": "2_Socio-Religious_Reform_Movements_Part-2/ModernIndianHistory-SocioReforms-Module2-1555338156.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1GMOHoogOmqSBtNWitCkwZ?si=9EiHwj7kTvepSUXR4BPykQ"
+    },
+    {
+        "name": "3_Socio-Religious_Reform_Movements_Part-3",
+        "pdf": "3_Socio-Religious_Reform_Movements_Part-3/ModernIndianHistory-Socio-Reforms-Module3-1555424526.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4WCuiKGHT7CoGJHxWxF9VE?si=cveKA-AtTmeH2XZrq-qTDw"
+    },
+    {
+        "name": "4_Socio-Religious_Reform_Movements_Part-4",
+        "pdf": "4_Socio-Religious_Reform_Movements_Part-4/ModernIndianHistory-Socio-Reforms-Module4-1555515741.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1H4cq9C6DOPJyomdHmPRLN?si=z9C0oOrhS36XHFVD5Jg_fA"
+    },
+    {
+        "name": "5_Socio-Religious_Reform_Movements_Part-5",
+        "pdf": "5_Socio-Religious_Reform_Movements_Part-5/ModernIndianHistory-Socio-Reforms-Module5-1556031244.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1AcPr7rDsJsmmI40cmfotZ?si=PBmAx12uS8mq0fGXlQ7brg"
+    },
+    {
+        "name": "6_Revolutionary_Movements_part-1",
+        "pdf": "6_Revolutionary_Movements_part-1/ModernIndianHistory-ReolutionaryMovements-Module1-1555755012.pdf",
+        "spotify_link": "https://open.spotify.com/episode/22EgjXmiNxLAd72PXO0j47?si=awOb_lsrTO2Jwd1ZPAAo_A"
+    },
+    {
+        "name": "7_Revolutionary_Movements_part-2",
+        "pdf": "7_Revolutionary_Movements_part-2/ModernIndianHistory-RevolutionaryMovements-Module2-1556028196.pdf",
+        "spotify_link": "https://open.spotify.com/episode/74LkOg8XU9z5nJlG12n94r?si=QdPgu6Y7QQ229-PNe08p-g"
+    },
+    {
+        "name": "8_Revolutionary_Movements_part-3",
+        "pdf": "8_Revolutionary_Movements_part-3/ModernIndianHistory-RevolutionaryMovements-Module3-1556198448.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5zQEZcLjpbn3gCKmYU5nrV?si=vgh8AYjVRP2LU0DK6CQFUA"
+    },
+    {
+        "name": "9_Civils_and_Tribal_Uprisings",
+        "pdf": "9_Civils_and_Tribal_Uprisings/Presentation1-1556544167.pdf",
+        "spotify_link": "https://open.spotify.com/episode/13yYma4gKbWzbcA9woJJTU?si=Zr7Jcm0ATzSgSbTYbWk4Cw"
+    },
+    {
+        "name": "10_Peasant_Movements",
+        "pdf": "10_Peasant_Movements/PeasantMovements-1557229169.pdf",
+        "spotify_link": "https://open.spotify.com/episode/58NIweeCo2nqVA43Jz6ytz?si=NreRdTqeToSaxVJlZTNELA"
+    },
+    {
+        "name": "11_Leftist_Movements",
+        "pdf": "11_Leftist_Movements/MODERNINDIANHISTORYMOVEMENT-5-1556793706.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1d9sUz3LSwdnKwPq8b1Oxj?si=-vsRErHTSZehy1aYUSnFfQ"
+    },
+    {
+        "name": "12_1857_revolt",
+        "pdf": "12_1857_revolt/Modern Indian History - 1857 Revolt - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7s5URvmjSKZiNwYk2mOxLA?si=0En8aPfoQTeapcd9c9jkSQ"
+    }
+];
