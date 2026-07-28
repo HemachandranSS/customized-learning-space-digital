ce7ad94e847482d4b07f2e14c66d47741359260f
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 18:16:22 +0530

    Added Ethics, Integrity & Aptitude

---
+++ ethics/ethics-topics.js
@@ -0,0 +1,282 @@
+const topics = [
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 1",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0K2K57x8f7JFIOwKk3S4sN?si=NZ-klEndTgmKms1Aa4-I6w"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 2",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity and Aptitude - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4cO9ImRvwN0cIWAfvIoDKZ?si=1E8sehuhR-6zlFKVrEP84Q"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 3",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2RsBtZG9B6X8rjnrD2Y9c6?si=P9htK6OWQ5CkoD8TIuFAGA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 4",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7a3z2jTGTfAHXB48nZVTGo?si=jw8XW-UCSoC2PElls0MfBQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 5",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5T8K9AVvHMfphc1BQZpBFu?si=6LP5QLwyR1eAxk7l4YWeog"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 6",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3orUDnhk8t461EdbwZejMm?si=IUJNkxlyT0GjuULIgJbC5w"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 7",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3XTva7wrh53RxnTT8aPscL?si=uivpMJkjSc-pSRR059ICSg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 8",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4JSMZQYPEFxeeRnJ1HRm27?si=aFValdLyRtqRjcEaTO6FSQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 9",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0pV4raFHv5zJqErN2ju44v?si=hIV8StVJS8ORnReDEQaTpg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 10",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2nDWHMIUgUbYe60ncbt93F?si=EBqgOIwuT4WcZpX5XceNng"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 11",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3CFonmLCZJjIYG6Wcg1B4O?si=sgNoxrZGQpWOe0Y5fzhN1A"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 12",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5o9RvnxoRZ3tGZKRTi4NUD?si=paaUCO2WQm6uZ-234ILjdQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 13",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4zlGr3LHJeZjRa3o8ROz9e?si=peEiCsh0Tg-FHWlTRzVKmA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 14",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4D2Mc5jTbbTeB2CUveJPhy?si=97k2rPkYSsWRzJ0JcGg2wg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 15",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/61Wek6J6m0TAKcc5NJOeQj?si=_saLmQ4vRrGNt9Jvqih9Ag"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 16",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2ETv6fld3F8Ww63Ul96xEM?si=dT5e2gs6QeSACU-kSRiS-g"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 17",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4nwVW4WfkeRn5cf96IsNHF?si=xFzsFxYaQ361nk47FUp7OQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 18",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4teO8bZpi2PRBvu1An8NhN?si=ALAfWOJ3Q4O0l-HgvB49Sw"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 19",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3KsBToANcvxLjCW8zZzCPZ?si=7LiOYvpiSTinCRQoG2lJZA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 20",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0QPd3QQIvYfD7k39F87Szr?si=aJsn3ANxTMGuMURDlfAsNA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 21",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/06NcaX2QF0eC5sAHbBUG4z?si=j1OqHRXpS8i6N_ZOvXzdww"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 22",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/44B83P0LtmnBpbcVnzBzhk?si=m71G2-UoQfO-FTlOKQxLTQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 23",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 23.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2Iq6AFl2ULFFSu7cpUVdIU?si=nDP1yDroTb6wEV_x0o-vjQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 24",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 24.pdf",
+        "spotify_link": "https://open.spotify.com/episode/21mX9JLnCztsUw9XAxKLfD?si=82anR9izSvSwVAmkzsV6EQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 25",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 25.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4tds13O9ITvqvb9cUkEncp?si=kh7yrXfiRuOS0DJOhVShjQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 26",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 26.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4qPoXMyGsZrBOsqaPcrF8h?si=lPikFzm8S7yiwsDS60MWMg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 27",
+        "pdf": "Ethics,Integrity-and-Aptitute/Ethics, Integrity & Aptitude - Module 27.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2l9Qm9LuJIWObWQlYruIpB?si=UQH7fMTdTtqmYBACWCp4zA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 28",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 28.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0SxK6CpaKm6xVTurbFaSar?si=U1k3JUFFQyi04vLiJBz3fQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 29",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 29 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5E9x7srvfgRPPZXOG0P4EI?si=kzxspGMaT0m8rrYo3e8A7g"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 30",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 30 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6d02ZOeIrRfMlZJqvYF1Hj?si=A5qZda48SkqEZigMdboM_Q"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 31",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 31 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5gxYpjEQUGlrMSr1yTdkPh?si=7nwdEnh4R5yjI0ZsFXxH3Q"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 32",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 32 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2acVbzc1VmTMoWVM70nqrx?si=CVHFn8T_SPi9t5cFAQFo1Q"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 33",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 33 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1vyPUwrvrERByCr5GYltLN?si=wjf6DeKdRbKweOxXOyTVDQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 34",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 34 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2LXRDYddVgx1iYT526oHWE?si=yEMF8szwQ3yTpvyH0Ae5Hg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 35",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 35 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1602qjCJ7Ja6JpMs0jGcgi?si=kKOmffGiRRSGPUViFmpp2Q"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 36",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 36 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2qTaRX8y0ai4SJ1KbvVEaD?si=CP8OjMJOT-OnY7ax2NhBbA"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 37",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 37 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6cUvbrzfD0wrvc9D05RI8N?si=DwwMBmE-RoOWF5EpsUWpwQ"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 38",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 38 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0Bv1xRIbgi8lMbX1cBLxmX?si=BIMCmlYLQTG4ouo9Yxo-Kg"
+    },
+    {
+        "name": "Ethics, Integrity & Aptitude - Module 39",
+        "pdf": "Ethics,Integrity-and-Aptitute/ETHICS INTEGRITY & APTITUDE - 39 up.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4kiItlnV8ySWKfPldfBgDL?si=PfFCnC4IQHiz7Og9VJf_qg"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 1",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0SdZgwbClMWsJfmt8wjyIo?si=CJCpy9sbS4aUZdgAoIUD3A"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 2",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2ki3OZJBTfm6gJxHmAKy1c?si=tMvLYhv4Qga164AWk-An9w"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 3",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5tQw5lkPIkc9ljEqza0Lgj?si=J9LdUO8GSSOSQ1qWoC9MoA"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 4",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/59oYTeMxrlDGUHwzUa7JOs?si=Z1-VrGq1R_qabQkFRtVpfg"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 5",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3hR2Ek4b4G5xqPBj3PVclF?si=sCVndtjCSOe2ODduklmJgg"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 6",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/02B6qEe3nKSy01bnorXtQh?si=IldKMkJtQJ6EVKzLWsYPlw"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 7",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2uL9tA9EVHK59p5GqvU7Hl?si=4pv0w7tZT6KMTWxqdKKjcg"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 8",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/32GEvIjrhlc7TfK6mEamVw?si=8y7oVXpuQaKeOOziCIBoPQ"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 9",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0VerlZP3IhtywwSS5xOZHF?si=z--TGbeiS5S9QprrxY51Rw"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 10",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2YEv4cegUQp3JZ5hQEcq9U?si=f8MfE0_hSOuz9BUB3H3ytQ"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 11",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3xzlPYRJ1VCaIeYM3SYRNF?si=ztb3W5ywScetLLZmlbowHg"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 12",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1hiMAgoroMcPckApSiBxcC?si=8oYOvPQiRfexYpOo9Hbfnw"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 13",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0t0mtVL5KGZCtzCniEPGOS?si=9kRBwEEgSkGE0O27ADQEjQ"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 14",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0bA6b0xsuhOYzpaEH0nxeU?si=6qAbXrynRgm57uVXqm6spQ"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 15",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3PnW9CoZxSI3Su95nYKESj?si=7EKyJ22VTg-wVxqv-mSdxw"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 16",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6qWWa13xP25IrtLCzpZAJ5?si=6YU4IJX_SC2s8p-vWia04g"
+    },
+    {
+        "name": "Mains Ethics Case Studies - Module 17",
+        "pdf": "Mains-Ethics-Case-Studies/UPSC Mains -Ethics Case Studies - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/26ORwRQLFnpN1FFUaHj66n?si=oPlueQ-qT5yfQ0pBTZXxFw"
+    }
+];
