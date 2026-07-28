d6f8633e5d57964c3d31c42986e282bfcab4dc37
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 14:06:26 +0530

    Added Oceanography

---
+++ geography-oceanography/geography-oceanography-topics.js
@@ -0,0 +1,42 @@
+const topics = [
+    {
+        "name": "1_Oceanography_-_Module_1",
+        "pdf": "1_Oceanography_-_Module_1/Oceanography-Module1-1545653835.pdf",
+        "spotify_link": "https://open.spotify.com/episode/66wTeqVGhh8VBi6Nklykbc?si=QyJm5f3HT7GboLr6smHGEA"
+    },
+    {
+        "name": "2_Oceanography_-_Module_2",
+        "pdf": "2_Oceanography_-_Module_2/Oceanography-Module2-1545914556.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7zJofFjGR381C56MQfNAY7?si=0CpBFcxSSsuthp7uZHU1Qw"
+    },
+    {
+        "name": "3_Oceanography_-_Module_3",
+        "pdf": "3_Oceanography_-_Module_3/Oceanography-Module3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2Stx4CLV7H0jx2xdSHDxgO?si=1xJfMn7PRLinxwrT7fYfpA"
+    },
+    {
+        "name": "4_Oceanography_-_Module_4",
+        "pdf": "4_Oceanography_-_Module_4/Oceanography - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7KSTjxZg97Dmsk3VlmuU7h?si=WAVPQCM2S6SNE-lOiU7kdQ"
+    },
+    {
+        "name": "5_Oceanography_-_Module_5",
+        "pdf": "5_Oceanography_-_Module_5/Oceanography-Module5-1547037128.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6ltTELs2QzGK21YDoQ8aiy?si=UgXfWrxvStOJfZ-xQzXeFg"
+    },
+    {
+        "name": "6_Oceanography-_Module_6",
+        "pdf": "6_Oceanography-_Module_6/Oceanography-Module6-1547783909.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2phUKaaFdAYJMrZnCkG9kR?si=Me6ED5XuQl6SgIv7KwJu5Q"
+    },
+    {
+        "name": "7_Oceanography_-_Module_-_7",
+        "pdf": "7_Oceanography_-_Module_-_7/Oceanography-Module-7-1548419746.pdf",
+        "spotify_link": "https://open.spotify.com/episode/31d98s3Pv2XApyVNrdl6ED?si=Q-Ox54vtQ_6uD0vA3PSFGg"
+    },
+    {
+        "name": "8_Oceanography_-_Module_-_8",
+        "pdf": "8_Oceanography_-_Module_-_8/Oceanography-Module-8-1548420348.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4toBHODez2kfSHRZ2HqBf6?si=nBYZREqPQ9aHv3uBvj_4Cg"
+    }
+];
