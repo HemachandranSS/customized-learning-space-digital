798d6fe09428d20d63df908901c49f91166cce9d
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 13:56:10 +0530

    Added Indian Geography

---
+++ geography-indian-geography/geography-indian-geography-topics.js
@@ -0,0 +1,207 @@
+const topics = [
+    {
+        "name": "1_Indian_Geography_-_Module_1",
+        "pdf": "1_Indian_Geography_-_Module_1/IndianGeography-Module1-1536755762.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6JI8MBTLXXM99l5Cn4Mb6P?si=V0uHKTvwQ0qnXezyiFZRCQ"
+    },
+    {
+        "name": "2_Indian_Geography_-_Module_2",
+        "pdf": "2_Indian_Geography_-_Module_2/IndianGeography-Module2-1536756528.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6LoZcvm4E1JMCApK9Q2djg?si=Cdz2OQV5SYCPPotqvmv7QQ"
+    },
+    {
+        "name": "3_Indian_Geography_-_Module_3",
+        "pdf": "3_Indian_Geography_-_Module_3/IndianGeography-Module3-1537272760.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2N38ba3SP0wemCfYvX4oAH?si=v5KhFnrTQO6UXXgX7Xzv2g"
+    },
+    {
+        "name": "4_Indian_Geography_-_Module_4",
+        "pdf": "4_Indian_Geography_-_Module_4/IndianGeography-Module4-1537272948.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7aBp1zRQ0HUsZmwkGS499C?si=3Uuef-DxTjWbUICRYS0zNA"
+    },
+    {
+        "name": "5_Indian_Geography_-_Module_5",
+        "pdf": "5_Indian_Geography_-_Module_5/IndianGeography-Module5-1537961216.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0Ruj2JRCqa2OLgg3XA4fdB?si=p64WDpw-TVKKBctxF-oaCg"
+    },
+    {
+        "name": "6_Indian_Geography_-_Module_6",
+        "pdf": "6_Indian_Geography_-_Module_6/IndianGeography-Module6-1539091512.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0L0jJmeRUzZyQjWI9vZXba?si=DguXTVTsSM2wUGVGUhBmNQ"
+    },
+    {
+        "name": "7_Indian_Geography_-_Module_7",
+        "pdf": "7_Indian_Geography_-_Module_7/IndianGeography-Module7-1539163429.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6iROUxMTM1lEeqUL5LQOIA?si=YGOV06AgQ2OPF0ZdUOcg4Q"
+    },
+    {
+        "name": "8_Indian_Geography_-_Module_8",
+        "pdf": "8_Indian_Geography_-_Module_8/IndianGeography-Module8-1539264345.pdf",
+        "spotify_link": "https://open.spotify.com/episode/73Lmtd7zK94QoBr2Ep45Lw?si=HGJszFDsQVSHSwFW3JvFrw"
+    },
+    {
+        "name": "9_Indian_Geography_-_Module_9",
+        "pdf": "9_Indian_Geography_-_Module_9/IndianGeography-Module9-1539347745.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0eVwACGUeeiEfFeKaWhl94?si=tb3yh1R5Tqq7-KLKMWt_RQ"
+    },
+    {
+        "name": "10_Indian_Climate_-_Module_1",
+        "pdf": "10_Indian_Climate_-_Module_1/IndianClimate-Module1-1540558071.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5f6tuiE90ADvrufmSe1haa?si=vZPp9MaSS8GI0nnfLfCHUQ"
+    },
+    {
+        "name": "11_Indian_Climate_-_Module_2",
+        "pdf": "11_Indian_Climate_-_Module_2/IndianClimate-Moduel2-1540988885.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4OR9YhCHixjixpDuW8Eclh?si=Etqn8TC7QDumY2LeYp5VZg"
+    },
+    {
+        "name": "12_Indian_Climate_-_Module_3",
+        "pdf": "12_Indian_Climate_-_Module_3/IndianClimate-Module3-1541164582.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4XaycYZqnFLfEK2k720iHc?si=Vjh94O9LRhCIHPlxg09WGQ"
+    },
+    {
+        "name": "13_Indian_Climate_-_Module_4",
+        "pdf": "13_Indian_Climate_-_Module_4/IndianGeography-Module4-1541679684.pdf",
+        "spotify_link": "https://open.spotify.com/episode/434OCPzWyclnxJBWcTrGE5?si=YmsgKiOHSWG47BWyi6l-Sg"
+    },
+    {
+        "name": "14_Indian_Climate_-_Module_5",
+        "pdf": "14_Indian_Climate_-_Module_5/IndianClimate-Module5-1542113347.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0kUvkmGBN9BC2sPL2KjtIc?si=BiSbtaDBTRWGklfPM_-2oA"
+    },
+    {
+        "name": "15_Indian_Climate_-_Module_6",
+        "pdf": "15_Indian_Climate_-_Module_6/IndianClimate-Module6-1542283706.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2o7FiGrfKC88jIiWxycxeD?si=znREw3Q4TE-5RZDqwQIOOg"
+    },
+    {
+        "name": "16_Indian_Climate_-_Module_7",
+        "pdf": "16_Indian_Climate_-_Module_7/IndianClimate-Module7-1542376343.pdf",
+        "spotify_link": "https://open.spotify.com/episode/48QOY9er0CmW9Q1JPXsQU8?si=drc-CVoyQHKcGVj4rdmDIg"
+    },
+    {
+        "name": "17_Indian_Natural_Vegetation_-_Module_1",
+        "pdf": "17_Indian_Natural_Vegetation_-_Module_1/IndianNaturalVegetation-Module1-1542718206.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0bJegWCy04TOTdLlP6asIZ?si=BFsNKNINR4-loz9xBPKDtQ"
+    },
+    {
+        "name": "18_Indian_Natural_Vegetation_-_Module_2",
+        "pdf": "18_Indian_Natural_Vegetation_-_Module_2/IndianNaturalVegetation-2-Module2-1542803684.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3xoHO6fy6ROOwdjWOgl1KL?si=g0lzvj_ETeGoFzJyHYMuLw"
+    },
+    {
+        "name": "19_Indian_Natural_Vegetation_-Module_3",
+        "pdf": "19_Indian_Natural_Vegetation_-Module_3/IndianNaturalVegetation-Module3-1542894557.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4IPzGNBc9KIrWYUAGFcl3u?si=MvYSLZZVSmm7OntI1h6Waw"
+    },
+    {
+        "name": "20_Indian_Physiography_-_Module_1",
+        "pdf": "20_Indian_Physiography_-_Module_1/IndianPhysiography-Module1-1543410886.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7EwwA5ACViHiCYJtBWxCbb?si=76fzaym_R-GvwcNg5zHvwA"
+    },
+    {
+        "name": "21_Indian_Physiography_-_Module_2",
+        "pdf": "21_Indian_Physiography_-_Module_2/IndianPhysiography-Module2-1543927780.pdf",
+        "spotify_link": "https://open.spotify.com/episode/78K688hsCtG6NZm19A6nqc?si=1m7beYMuS7unrmsLCBPEHg"
+    },
+    {
+        "name": "22_Indian_Physiography_-_Module_3",
+        "pdf": "22_Indian_Physiography_-_Module_3/IndianPhysiography-Module03-1544448609.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1vWoOJJ3tGbdn2iUIN4Xcu?si=IrZcVD-XQ4eQ3JrvPkG5bg"
+    },
+    {
+        "name": "23_Indian_Physiography_-_Module_4",
+        "pdf": "23_Indian_Physiography_-_Module_4/IndianPhysiography-Module4-1544785969.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3K6dca32QMjY1jaNnhlNK0?si=U2ZusVTwRLq9QGagfcYI-A"
+    },
+    {
+        "name": "24_Indian_Physiography_-_Module_5",
+        "pdf": "24_Indian_Physiography_-_Module_5/IndianPhysiography-Module5-1545048970.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1lkIAr1q0gzPB2q1xiCUte?si=f1zpl1hfTzWsC145xKI_IQ"
+    },
+    {
+        "name": "25_Indian_Physiography_-_Module_6",
+        "pdf": "25_Indian_Physiography_-_Module_6/IndianPhysiography-Module6-1545310018.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4djjVwPmwqcP2T8lU1MLuY?si=tCJEq98HRcKiLHgqy93nEQ"
+    },
+    {
+        "name": "26_Indian_Physiography_-Module_7",
+        "pdf": "26_Indian_Physiography_-Module_7/IndianPhysiography-Module7-1546260968.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1v3JdiDzIGeL6A0itSwIOT?si=uPbCTGyuQASXk8mmTxfe6g"
+    },
+    {
+        "name": "27_Indian_Physiography_-_Module_8",
+        "pdf": "27_Indian_Physiography_-_Module_8/IndianPhysiography-Module8-1546434630.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5g8sgK0JcbaetvNnRKYQJl?si=9t_zigZjRt2ZlgFbJ4nnXg"
+    },
+    {
+        "name": "28_Indian_Physiography_-_Module_9",
+        "pdf": "28_Indian_Physiography_-_Module_9/IndianPhysiography-Module9-1546604907.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3hqx4c75fS2ufNdgJmj4ys?si=_Ivem3zgSDG_Jke4De6hvg"
+    },
+    {
+        "name": "29_Indian_Drainage_System_-_Module_1",
+        "pdf": "29_Indian_Drainage_System_-_Module_1/IndianDrainageSystem-Module1-1546953711.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3j14vf9abJKR8A9E3WJuYC?si=zwM-XovQQW-g1dYul2dNSA"
+    },
+    {
+        "name": "30_Indian_Drainage_System_-_Module_2",
+        "pdf": "30_Indian_Drainage_System_-_Module_2/IndianDrainageSystem-Module2-1547807596.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0O6BO6QrHGaDndOjoeMIwJ?si=YPOqhpnHTvSpuqRUOxqg2A"
+    },
+    {
+        "name": "31_Indian_Drainage_System_-_Module_3",
+        "pdf": "31_Indian_Drainage_System_-_Module_3/IndianDrainageSystem-Module3-1548335384.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1GWtd9jRpSy9T56H0gyMlu?si=DyPX0m5cSTGEECpzQdjhLg"
+    },
+    {
+        "name": "32_Indian_Drainage_System_-_Module_4",
+        "pdf": "32_Indian_Drainage_System_-_Module_4/IndianDrainageSystem-Module4-1548682400.pdf",
+        "spotify_link": "https://open.spotify.com/episode/08F02HJ6zqHZegji0YiZJV?si=tVNbG9q8SU68W9i_UiDb5Q"
+    },
+    {
+        "name": "33_Minerals_and_Industries_-_Module_1",
+        "pdf": "33_Minerals_and_Industries_-_Module_1/MineralsandIndustries-Module1-1548767866.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2slcW2wJBdbGjkTTYMIK0B?si=6aqoiiizRiKCzqYHgPVUJA"
+    },
+    {
+        "name": "34_Minerals_and_Industries_-_Module_2",
+        "pdf": "34_Minerals_and_Industries_-_Module_2/MineralsIndustries-Module2-1548852908.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7e9NGVCH6eKVYSDHPMbNBa?si=PvH9e7X1RgGyaa-ukAd1UQ"
+    },
+    {
+        "name": "35_Minerals_and_Industries_-_Module_3",
+        "pdf": "35_Minerals_and_Industries_-_Module_3/MineralsandIndustries-Module3-1549025262.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4CHlU7rG4GhskAMcrZMLSK?si=BmMbWirIRW2NHk2MF4kMDQ"
+    },
+    {
+        "name": "36_Minerals_and_Industries_-_Module_4",
+        "pdf": "36_Minerals_and_Industries_-_Module_4/MineralsandIndustries-Module4-1549282572.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1FfUBTcsbzGY9kNUDbVnWj?si=swdHOpGdQF60gtWBEDoKUQ"
+    },
+    {
+        "name": "37_Minerals_and_Industires_-Module_5",
+        "pdf": "37_Minerals_and_Industires_-Module_5/MineralsandIndustires-Module5-1549375340.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5qPNot8GpU28bc3cH6Sj7O?si=c1Zt0iK8T6O16WbhjCxzzw"
+    },
+    {
+        "name": "38_Minerals_and_Industires_-Module_6",
+        "pdf": "38_Minerals_and_Industires_-Module_6/INDIANGEOGRAPHYmineralindustir-6-1549456799.pdf",
+        "spotify_link": "https://open.spotify.com/episode/379heY12RBUlf5RmKhUraA?si=R9O45AAaRZuv0eI2SKa64A"
+    },
+    {
+        "name": "39_Minerals_and_Industries_-_Module_7",
+        "pdf": "39_Minerals_and_Industries_-_Module_7/MineralsandIndustries-Module7-1549544222.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5jV6MxS6kDsKlsn5NGhUZp?si=TxhWZR4nS7Webw_s4r-zkg"
+    },
+    {
+        "name": "40_Minerals_and_Industries_-_Module_8",
+        "pdf": "40_Minerals_and_Industries_-_Module_8/MineralsandIndustries-Module8-1549978593.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0CjJ95E7soC1uaHz6jew7X?si=yUofYsoCS3uTzeSMo82kjg"
+    },
+    {
+        "name": "41_Minerals_and_Industries_-_Module_9",
+        "pdf": "41_Minerals_and_Industries_-_Module_9/MineralsandIndustries-Module9-1549978969.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0hrnsMjQu9Q2PPU7DlhVf6?si=j7TuuZHpSXqEWa7eBbHRow"
+    }
+];
