4ec75b0be26c0d9bfff377356872acac27c18d04
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 15:19:16 +0530

    Added Internal Security

---
+++ internal-security/internal-security-topics.js
@@ -0,0 +1,112 @@
+const topics = [
+    {
+        "name": "Internal Security - Module 1",
+        "pdf": "Internal Security - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7bVxwkCDFCquOvKDYtHL6y?si=kcSZ3J-YQniGGaeAWrzM6g"
+    },
+    {
+        "name": "Internal Security - Module 2",
+        "pdf": "Internal Security - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/36Ap1dMulOvkzEXoHx9AY0?si=nDQz5ibcS8qKyXdwARauJA"
+    },
+    {
+        "name": "Internal Security - Module 3",
+        "pdf": "Internal Security - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4XFoSSjpW2GgBW85252joA?si=7rKpH2UGQYu38E6ao82TNw"
+    },
+    {
+        "name": "Internal Security - Module 4",
+        "pdf": "Internal Security - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4pWeRyyTegxKbunELRwR6v?si=LGhUtZuZS5KtKjT8kq7acA"
+    },
+    {
+        "name": "Internal Security - Module 5",
+        "pdf": "Internal Security - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5xEEwYQRWqy9fE1SvY5t14?si=lpHHIVy4RZyv4B0oitnmUw"
+    },
+    {
+        "name": "Internal Security - Module 06",
+        "pdf": "Internal Security - Module 06.pdf",
+        "spotify_link": "https://open.spotify.com/episode/48InOkLjod5qXXxKIFKWrF?si=TEx-ACi3ToeEgzRwJVwMjg"
+    },
+    {
+        "name": "Internal Security - Module 7",
+        "pdf": "Internal Security - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3hNGdRIoz8cEs6xrkTPULH?si=wfu9ipKrTXCIBM8py1p_Xg"
+    },
+    {
+        "name": "Internal Security - Module 8",
+        "pdf": "Internal Security - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6XboAHICJ6HOh08lPob8kM?si=8S8eMCaLTUmV8Ia2omEpFg"
+    },
+    {
+        "name": "Internal Security - Module 09",
+        "pdf": "Internal Security - Module 09.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4dwDpFWJbyv2KIdyT5uv9A?si=8p7suYd-Q1Wd-dl8OlcdGw"
+    },
+    {
+        "name": "Internal Security - Module 10",
+        "pdf": "Internal Security - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6xtYJYQwMGJAT9NWwoxLrM?si=kJ0b5DfdRl6dJbJZCXd-Fg"
+    },
+    {
+        "name": "Internal Security - Module 11",
+        "pdf": "Internal Security - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7uLDxTnyqTUzvWyCW3p00M?si=LErBbcioRbWSYV05W0ixnQ"
+    },
+    {
+        "name": "Internal Security - Module 12",
+        "pdf": "Internal Security - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1FFxvOKA9KA33VfQ677wdv?si=VDxJbqjJRH-wPzUZstbUuQ"
+    },
+    {
+        "name": "Internal Security - Module 13",
+        "pdf": "Internal Security - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4PlVZnXJazOptwLUptIKUf?si=mj7uED2TSWaIXwi3nbnzdw"
+    },
+    {
+        "name": "Internal Security - Module 14",
+        "pdf": "Internal Security - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4zCrBso0S36JPHHaHfoQxg?si=ksoXmvtORIqhKod0stCLrQ"
+    },
+    {
+        "name": "Internal Security - Module 15",
+        "pdf": "Internal Security - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3ZZKFfBkV72LwyLjY6dQLs?si=mIxTRDUVSCCcsmLipA0rZg"
+    },
+    {
+        "name": "Internal Security - Module16",
+        "pdf": "Internal Security - Module16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2oXwTDYS6lziItne3gxUVN?si=40fOtXC4Q7q8ccQ9fz66JA"
+    },
+    {
+        "name": "Internal Security - Module 17",
+        "pdf": "Internal Security - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/24BRSUl7iRcyHe7cQl4d8s?si=lZSXoPnhS9qecqTE0OaIRg"
+    },
+    {
+        "name": "Internal Security - Module 18",
+        "pdf": "Internal Security - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6RvglVLLQlG68ccfkJxdRU?si=_OuUggNYTJSUip22L-xfLg"
+    },
+    {
+        "name": "Internal Security - Module 19",
+        "pdf": "Internal Security - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4KGagcoBQudItB5U75lwcm?si=CyvVZO3JReWRxvXV447Oqg"
+    },
+    {
+        "name": "Internal Security - Module 20",
+        "pdf": "Internal Security - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3uaIaCcNec2r9JGXlAFzuR?si=uBjmEaZSQLS2TLBVQxNuRA"
+    },
+    {
+        "name": "Internal Security - Module 21",
+        "pdf": "Internal Security - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2zafOfzgYggOKT0ZOw5dBW?si=kHWMkrOdSJ--81zXq88LSA"
+    },
+    {
+        "name": "Internal Security - Module 22",
+        "pdf": "Internal Security - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/40BqHOKi9n1cGYnsbv8Qx2?si=kTtGW7qnRauxve5P3l8hvQ"
+    }
+];
