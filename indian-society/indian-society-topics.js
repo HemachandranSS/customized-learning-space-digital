8cfe05434009ebd6567c2e5a968dc674534c3c95
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 10:18:51 +0530

    Jul 28 , 2026 - Added Indian Society

---
+++ indian-society/indian-society-topics.js
@@ -0,0 +1,77 @@
+const topics = [
+    {
+        "name": "Indian Society - Module 1",
+        "pdf": "Indian Society - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5mfukXqEww4s6HAtjsvw5X?si=7cObOgkRSV6ZxgIobHCmmA"
+    },
+    {
+        "name": "Indian Society - Module 2",
+        "pdf": "Indian Society - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3cbB0uS6oDvHMFTVJXMEnX?si=EN5XRdKOQvSWm-c0v0g-Lw"
+    },
+    {
+        "name": "Indian Society - Module 3",
+        "pdf": "Indian Society - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1ChAP0jsiKuyc13W4AzV2J?si=_6rmBpouS7-TMUsK8bmQiw"
+    },
+    {
+        "name": "Indian Society - Module 4",
+        "pdf": "Indian Society - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0cx4AronkMqOjjS4LTBSnJ?si=0KhV8vh_QcCTJ_7k8ETaqQ"
+    },
+    {
+        "name": "Indian Society - Module 5",
+        "pdf": "Indian Society - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2TT5KGGfssIrX5B39uRnad?si=HGhnTiqGTnGci0SEUkjphg"
+    },
+    {
+        "name": "Indian Society - Module 6",
+        "pdf": "Indian Society - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5zKuEk6t1RDFai4IT0biB0?si=x7oxjjb6QAiOkEa-1ajIZg"
+    },
+    {
+        "name": "Indian Society - Module 7",
+        "pdf": "Indian Society - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3RxDGyvLhd7EbEBC5mNodp?si=GAEqLmItR-Kjvuriku_Beg"
+    },
+    {
+        "name": "Indian Society - Module 8",
+        "pdf": "Indian Society - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4QEgOzVHycKlsJOgV8UC9I?si=12qKwMPNTv-3UFi5f6-sAw"
+    },
+    {
+        "name": "Indian Society - Module 9",
+        "pdf": "Indian Society - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4lynCOiTdKWOxx32JVesd3?si=mgEnklbwToOU6y0qbqSpuw"
+    },
+    {
+        "name": "Indian Society - Module 10",
+        "pdf": "Indian Society - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5ZQuQBevUQL0EVZyctQ6No?si=kVM4ILKsQuK8oD3XwefIQQ"
+    },
+    {
+        "name": "Indian Society - Module 11",
+        "pdf": "Indian Society - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2XGlg2qlYTkdqquS7HLlFI?si=yFrT1o5lQhSjbHPvVCpaKQ"
+    },
+    {
+        "name": "Indian Society - Module 12",
+        "pdf": "Indian Society - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5IvoR9rKUstuQegeoyIi38?si=heQQDsffRxCfFtacpqPLcg"
+    },
+    {
+        "name": "Indian Society - Module 13",
+        "pdf": "Indian Society - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/384HqzCFm2QEbmZqGKGVGj?si=gswEmxZxSSyrF4DAcp9JEg"
+    },
+    {
+        "name": "Indian Society - Module 14",
+        "pdf": "Indian Society - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1tKxWlkWvOcVmtXmxqq2nC?si=HGgFHVpZQQemwjL8YLeKiw"
+    },
+    {
+        "name": "Indian Society - Module 15",
+        "pdf": "Indian Society - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5vWULKkgBSe7ITszb7Qrtx?si=cPwz_tlbQGe9Ev9u8PnVyg"
+    }
+];
