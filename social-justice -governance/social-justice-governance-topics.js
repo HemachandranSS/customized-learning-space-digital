7130f3e6ea6ae9e0c7d328d587cc0dcdab715b6b
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 10:34:11 +0530

    Added Social Justice and Governance

---
+++ social-justice -governance/social-justice-governance-topics.js
@@ -0,0 +1,132 @@
+const topics = [
+    {
+        "name": "Governance - Introduction",
+        "pdf": "Governance - Introduction.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5qQZbBzgr0VLhwsUHdVFYt?si=1cnVtj9URsGYdhKk_2obuQ"
+    },
+    {
+        "name": "Governance - Module 1",
+        "pdf": "Governance - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3Er6UB8nhusjAnxkGVobty?si=77haao9ASCiY-OAYQ-RjmA"
+    },
+    {
+        "name": "Governance - Module 2",
+        "pdf": "Governance - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6rYNA7sp7ZuW8ysp9Mztpv?si=XvH0Vbv4QjuSkwpCPr5aqg"
+    },
+    {
+        "name": "Governance - Module 3",
+        "pdf": "Governance - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2qamK3Dv3pl0JHqM8ECWDo?si=2h0it9Q8QduVdaxwWf3f9g"
+    },
+    {
+        "name": "Governance - Module 4",
+        "pdf": "Governance - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4WgCuRwhBHquSBJyC4DPVM?si=2WO-AFkvTZGzW30fA0f3PQ"
+    },
+    {
+        "name": "Governance - Module 5",
+        "pdf": "Governance - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5cXqhT7pU6xEEQHOU7zEjC?si=ESOpoVGATBSqo5D1zYu_fg"
+    },
+    {
+        "name": "Governance - Module 6",
+        "pdf": "Governance - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6rQlLqF9Uae7uDfqKIZO09?si=3fW13wKBSWOaG_U0oISDeg"
+    },
+    {
+        "name": "Governance - Module 7",
+        "pdf": "Governance - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/79MEN6ZkXje8nWuoxjqPJH?si=ODXGJ5gsR_i5ko-O6CxKUw"
+    },
+    {
+        "name": "Governance - Module 8",
+        "pdf": "Governance - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7Em4WVrTjkRuQXlvHdQyyj?si=bofXjxfOSeGbbFnx_SPy-Q"
+    },
+    {
+        "name": "Governance - Module 9",
+        "pdf": "Governance - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3S1v8mwVAoQGsNgqCApf2B?si=tdkazPB0R36c1LVCKMbc6g"
+    },
+    {
+        "name": "Governance - Module 10",
+        "pdf": "Governance - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3DarNkvmZp1bm36OeSwcLj?si=CwwQVWHBQmC045JPdRUmAg"
+    },
+    {
+        "name": "Governance - Module 11",
+        "pdf": "Governance - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/09Q3r4vo6ptiA7OSnPqrw7?si=Ai6rer0TRuGjKpHqZ0MUQg"
+    },
+    {
+        "name": "Governance - Module 12",
+        "pdf": "Governance - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5OebmUR7FVvSntXBlsI8qX?si=RxP50oP_QTKtj4Y_55u8aQ"
+    },
+    {
+        "name": "Governance - Module 13",
+        "pdf": "Governance - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7npI6GjcguCl9193tNVlXq?si=MmDOknooTSqrKZtflEld_g"
+    },
+    {
+        "name": "Governance - Module 14",
+        "pdf": "Governance - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1btLjTUkbbwDBYqRtK4FML?si=lNM_ipDlR_OTI1m0uifGug"
+    },
+    {
+        "name": "Governance - Module 15",
+        "pdf": "Governance - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3ZP2RGfTh43VTyfkTltaAp?si=orRclpBCR-em9wnnGje-5g"
+    },
+    {
+        "name": "Governance - Module 16",
+        "pdf": "Governance - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/05j8gVQet2oPh2MtVGn6Ot?si=szRgpf8VSA6TyTtIK-EabA"
+    },
+    {
+        "name": "Governance - Module 17",
+        "pdf": "Governance - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0kQDjzOQii7d3FxV7XBW7X?si=odS3c1GPQsagbShr-SQuTw"
+    },
+    {
+        "name": "Governance - Module 18",
+        "pdf": "Governance - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6ra4P0DGpw4wk88AkoWK02?si=X_gY19I6SUutMSF2u6W8Wg"
+    },
+    {
+        "name": "Governance - Module 19",
+        "pdf": "Governance - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4ZoMWcq1cYXV5qVOWEukQA?si=X0_Yz_QJTwCs65-SVH5J0A"
+    },
+    {
+        "name": "Governance - Module 20",
+        "pdf": "Governance - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/01xqnS9MZnjJckBvdlzGq9?si=6HmkYUReRQyb0W2v-nws0A"
+    },
+    {
+        "name": "Governance - Module 21",
+        "pdf": "Governance - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1FTcss0u37G4yq31JkwnWs?si=-bPN0ewCQxec8enWmYGATg"
+    },
+    {
+        "name": "Governance - Module 22",
+        "pdf": "Governance - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0MR5gz9BDeiJbIXKqByhzw?si=8k2UEdeOQ4yEjru2oohZmQ"
+    },
+    {
+        "name": "Governance - Module 23",
+        "pdf": "Governance - Module 23.pdf",
+        "spotify_link": "https://open.spotify.com/episode/14KrYHZl85ZjoPd80O2ljw?si=Flo92JgOQ3C7CsavyulhZQ"
+    },
+    {
+        "name": "Governance - Module 24",
+        "pdf": "Governance - Module 24.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6J1XBiyEiN4f2Hqe8ZJBlg?si=F2Lp1GsVTl6bG-D-68VWUQ"
+    },
+    {
+        "name": "Governance - Module 25",
+        "pdf": "Governance - Module 25.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5LnAjFp05uqUTk9RwQULAe?si=iD9rKCY9QUumP1hemFXwzQ"
+    }
+];
