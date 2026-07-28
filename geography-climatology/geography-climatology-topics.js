7b7297db766a0d719c719b23911c58486e9e8f6a
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 14:19:09 +0530

    Added Climatology

---
+++ geography-climatology/geography-climatology-topics.js
@@ -0,0 +1,242 @@
+const topics = [
+    {
+        "name": "1_Climatology_-_Module_1",
+        "pdf": "1_Climatology_-_Module_1/Cimatology-Module1-1521039463.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6Np8rui42DLlnEeLb7RFcN?si=YO5NqMYYSKCTgMQ67a5KMg"
+    },
+    {
+        "name": "2_Climatology_-_Module_2",
+        "pdf": "2_Climatology_-_Module_2/Climatology-Module2-1521282464.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7y6F42n2SsAopTTNjtiF5b?si=nRt8f0SlQuu0LPZ-hp8LqA"
+    },
+    {
+        "name": "3_Climatology_-_Module_3",
+        "pdf": "3_Climatology_-_Module_3/Climatology-Module3-1521295042.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5YkSrez8cCsP0pI65aFX1k?si=jkkkZ68pQomMxKG9cDTp7A"
+    },
+    {
+        "name": "4_Climatology_-_Module_4",
+        "pdf": "4_Climatology_-_Module_4/Climatology-Module4-1521631873.pdf",
+        "spotify_link": "https://open.spotify.com/episode/47Oj0Uo5PNV4GoFDx3Ji0b?si=omYnvAdRTcWfpJ2KWEmCjA"
+    },
+    {
+        "name": "5_Climatology_-_Module_5",
+        "pdf": "5_Climatology_-_Module_5/Climatology-Module5-1521729806.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3DiQ7PTnZF65mwzteCOdVe?si=ttGHXQggSWaslupAkQ62_w"
+    },
+    {
+        "name": "6_Climatology_-_Module_6",
+        "pdf": "6_Climatology_-_Module_6/Climatology-Module6-1521808722.pdf",
+        "spotify_link": "https://open.spotify.com/episode/39LV6UylMFmNWaOEFvBPm4?si=zsgT9KGxR0Si6hCwToxvSw"
+    },
+    {
+        "name": "7_Climatology_-_Module_7",
+        "pdf": "7_Climatology_-_Module_7/Climatology-Module7-1522072743.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4viqJYZoOGI9BFfevEVzhF?si=zGiwcDgBQzW9JxQ3tFLAtA"
+    },
+    {
+        "name": "8_Climatology_-_Module_8",
+        "pdf": "8_Climatology_-_Module_8/Climatology-Module8-1522155425.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4HGbgRcg3CTlEKwXC23FKz?si=E6whFz9DT4mB5PXYVkbNLw"
+    },
+    {
+        "name": "9_Climatology_-_Module_9",
+        "pdf": "9_Climatology_-_Module_9/Climatology-Module9-1522333085.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3s5DMmYkHV4erdLTzRiBCZ?si=x-Ga-63VSYK1T0MQPRj-Jg"
+    },
+    {
+        "name": "10_Climatology_-_Module_10",
+        "pdf": "10_Climatology_-_Module_10/Climatology-Module10-1522765513.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7diBeBDua7vmSrDu7LnXpr?si=5lrjXiNISdSgC8VC0Kva6w"
+    },
+    {
+        "name": "11_Climatology_-_Module_11",
+        "pdf": "11_Climatology_-_Module_11/Climatology-Module11-1522845659.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2UPsMYCAj0MRfD5PnKOhHA?si=BvhLp6cwRkWiYR3Vew2XnQ"
+    },
+    {
+        "name": "12_Climatology_-_Module_12",
+        "pdf": "12_Climatology_-_Module_12/Climatology-Module12-1522935517.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1r5jJU2sfxdFc67bpb1RFf?si=5T9xH2pDQFukhkd4N7K60Q"
+    },
+    {
+        "name": "13_Climatology_-_Module_13",
+        "pdf": "13_Climatology_-_Module_13/Climatology-Module13-1523083201.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4yTTL8hURCVJxKVyBAsTHu?si=v0i1-Io3THKbYjc2dx48Aw"
+    },
+    {
+        "name": "14_Climatology_-_Module_14",
+        "pdf": "14_Climatology_-_Module_14/Climatology-Module14-1523539172.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1V3AnYBOSDpB2TtSQBwDnS?si=LyenQTn5R1mE7rGEKDEzXQ"
+    },
+    {
+        "name": "15_Climatology_-_Module_15",
+        "pdf": "15_Climatology_-_Module_15/Climatology-Module15-1523621386.pdf",
+        "spotify_link": "https://open.spotify.com/episode/178lycVFIjlhBIuXBUQ3s1?si=Aww_U14ER2yF8AD-Zhumig"
+    },
+    {
+        "name": "16_Climatology_-_Module_16",
+        "pdf": "16_Climatology_-_Module_16/Climatology-Module16-1523701331.pdf",
+        "spotify_link": "https://open.spotify.com/episode/13G6PuCXezC1DZh0Lp3wjE?si=dhM9GMqiQ8yYrNrsz1nhyg"
+    },
+    {
+        "name": "17_Climatology_-_Module_17",
+        "pdf": "17_Climatology_-_Module_17/Climatology-Module17-1524307377.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6zD7mMznNPBS1CQmIRMrhI?si=w-fLngLlQ6GaI6uABm_AAQ"
+    },
+    {
+        "name": "18_Climatology_-_Module_18",
+        "pdf": "18_Climatology_-_Module_18/Climatology-Module18-1525269189.pdf",
+        "spotify_link": "https://open.spotify.com/episode/14iWA1QFmy7coUKYLm6D72?si=yyi1B4MwQsuuxJpsvXsBaA"
+    },
+    {
+        "name": "19_Climatology_-_Module_19",
+        "pdf": "19_Climatology_-_Module_19/Climatology-Module19-1529936413.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0bXvckQdjqXTV5qTyCs1ZU?si=g5XIL8S0TBCOUxvWUZZl2Q"
+    },
+    {
+        "name": "20_Climatology_-_Module_20",
+        "pdf": "20_Climatology_-_Module_20/Climatology-Module20-1529936595.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1YnqwkyCFSCuxcuQPkvr6g?si=euaKmd4YRJWcUO4oDTG5jA"
+    },
+    {
+        "name": "21_Climatology_-_Module_21",
+        "pdf": "21_Climatology_-_Module_21/Climatology-Module21-1530343330.pdf",
+        "spotify_link": "https://open.spotify.com/episode/24MgfRZhJcdkL8OW3t9OJL?si=bTD7mM13QlKan6_v4CVnHA"
+    },
+    {
+        "name": "22_Climatology_-_Module_22",
+        "pdf": "22_Climatology_-_Module_22/Climatology-Module22-1530621533.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5MzNTOm9ENCP0kg0fiXyI9?si=fvF9JKe2RBGgosFOU86LFg"
+    },
+    {
+        "name": "23_Climatology_-_Module_23",
+        "pdf": "23_Climatology_-_Module_23/Climatology-Module23-1530624847.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1tFkYaK5hDveckS3n09f43?si=zt9btz57QHqFBs6bR6sgJw"
+    },
+    {
+        "name": "24_Climatology_-_Module_24",
+        "pdf": "24_Climatology_-_Module_24/Presentation1-1530944166.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0iuan44uyKxP2zmjEBFB7t?si=68sW4diuRQqJhjk8LqBz_Q"
+    },
+    {
+        "name": "25_Climatology_-_Module_25",
+        "pdf": "25_Climatology_-_Module_25/Presentation1-1530944221.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3ljc9F7ZuCQQ9Bw7SXtPh9?si=TJiYtor3RauXgD9eirizFQ"
+    },
+    {
+        "name": "26_Climatology_-_Module_26",
+        "pdf": "26_Climatology_-_Module_26/Climatology-Module26-1531136358.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3BM9Ysyh3RlCBrPrVwT1wf?si=1KfwnYvjQTm0eRoWA0pd_Q"
+    },
+    {
+        "name": "27_Climatology_-_Module_27",
+        "pdf": "27_Climatology_-_Module_27/Climatology-Module27-1531136730.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2aSwyPLXHtzuQhVitjjRWh?si=r9GCmtjHRYS8BhK46TGyqA"
+    },
+    {
+        "name": "28_Climatology_-_Module_28",
+        "pdf": "28_Climatology_-_Module_28/Climatology-Module28-1531476444.pdf",
+        "spotify_link": "https://open.spotify.com/episode/29tjDGqTgVG9qBuJBdCLeL?si=-8YqdXfXQuKGkgwWPPev7Q"
+    },
+    {
+        "name": "29_Climatology_-_Module_29",
+        "pdf": "29_Climatology_-_Module_29/Climatology-Module29-1531556558.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3qM492nU0IHrgzHeRBaUIQ?si=-whV3dlPQjSadKQAByw-mQ"
+    },
+    {
+        "name": "30_Climatology_-_Module_30",
+        "pdf": "30_Climatology_-_Module_30/Presentation1-1532350278.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7m1icAW9m5Yq2pX5GQrg11?si=lrIjYJaOTwSASvfxFQA21g"
+    },
+    {
+        "name": "31_Climatology_-_Module_31",
+        "pdf": "31_Climatology_-_Module_31/Climatology-Module31-1532755605.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4X7CXjyONHFQpUhEwEZMdo?si=9lsbjqpoRqyBjSveyH-3vQ"
+    },
+    {
+        "name": "32_Climatology_-_Module_32",
+        "pdf": "32_Climatology_-_Module_32/Climatology-Module32-1532756202.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6gdq5udJfTDnbEd5MhMteM?si=1R7DfJ83R8eUrOmng2hb3g"
+    },
+    {
+        "name": "33_Climatology_-_Module_33",
+        "pdf": "33_Climatology_-_Module_33/Climatology-Module33-1533214571.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6ElzUpeiTVplgYUXb4iY3Q?si=EgUxoV9VTHK75jZSnjnRvA"
+    },
+    {
+        "name": "34_Climatology_-_Module_34",
+        "pdf": "34_Climatology_-_Module_34/Climatology-Module34-1533214906.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1CnXGHGWEYiWAnCo390ONT?si=NteFgG0CR-yYH5bSJ_eKLA"
+    },
+    {
+        "name": "35_Climatology_-_Module_35",
+        "pdf": "35_Climatology_-_Module_35/Climatology-Module35-1534164959.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4HiTVsLqjsNHSlnAbXRvmI?si=s6rUdl-UQTy392hfWZCSqg"
+    },
+    {
+        "name": "36_Climatology_-_Module_36",
+        "pdf": "36_Climatology_-_Module_36/Climatology-Module36-1534251145.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0csPyLRYbwyphha4tHd1VV?si=71y33QW0Q8yJV3tHl8i49Q"
+    },
+    {
+        "name": "37_Climatology_-_Module_37",
+        "pdf": "37_Climatology_-_Module_37/Climatology-Module37-1536668588.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7xkzOUr5LxfsKNrB57wuJh?si=N6Apw-rJQZe6j-LkvN9YIw"
+    },
+    {
+        "name": "38_Climatology_-_Module_38",
+        "pdf": "38_Climatology_-_Module_38/Climatology-Module38-1536668825.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7kNj51higc4ELNIV1rf0iJ?si=GCuGVMhRQBellTYa6WmrHQ"
+    },
+    {
+        "name": "39_Climatology_-_Module_39",
+        "pdf": "39_Climatology_-_Module_39/Climatology-Module39-1536752583.pdf",
+        "spotify_link": "https://open.spotify.com/episode/19QnngVQKFHkeXF1nPDAvm?si=s9LRaplbRQ6vm6vQYlpJ0Q"
+    },
+    {
+        "name": "40_Climatology_-_Module_40",
+        "pdf": "40_Climatology_-_Module_40/Climatology-Module40-1537446074.pdf",
+        "spotify_link": "https://open.spotify.com/episode/02ImNYgHYWJp4Usb6tbHyX?si=vzRPqG4qTfevct1zY2hVZQ"
+    },
+    {
+        "name": "41_Climatology_-_Module_41",
+        "pdf": "41_Climatology_-_Module_41/Climatology-Module41-1537876396.pdf",
+        "spotify_link": "https://open.spotify.com/episode/40fknlalHsVtfTLIL1nJKg?si=YjW4M1jGRaGGZOFejBy6Uw"
+    },
+    {
+        "name": "42_Climatology_-_Module_42",
+        "pdf": "42_Climatology_-_Module_42/Climatology-Module42-1537961014.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2mVrqX6phgpfwKAJye7VBm?si=TFggI00zSAe2LMmMcelqsg"
+    },
+    {
+        "name": "43_Climatology_-_Module_43",
+        "pdf": "43_Climatology_-_Module_43/Climatology-Module43-1538054990.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3kyDdoWUEJjHcRqr63Eirx?si=Cm3kB9NVRmKqOQvbK6knig"
+    },
+    {
+        "name": "44_Climatology_-_Module_44",
+        "pdf": "44_Climatology_-_Module_44/Climatology-Module44-1538055123.pdf",
+        "spotify_link": "https://open.spotify.com/episode/142Mvfr1yezrND7SlW4D2n?si=huzCvKi_RyarMOi_NZBKwQ"
+    },
+    {
+        "name": "45_Climatology_-_Module_45",
+        "pdf": "45_Climatology_-_Module_45/Climatology-Module45-1538055219.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6bNfyKWQzmkanhIlwfbdxJ?si=0RlGyG6aRbeAWXqkeQf1tw"
+    },
+    {
+        "name": "46_Climatology_-_Module_46",
+        "pdf": "46_Climatology_-_Module_46/Climatology-Module46-1538576038.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1txXvVPRGqC1ehLOY84OCI?si=qxHtwBjLSLW7sdJg7T7S9g"
+    },
+    {
+        "name": "47_Climatology_-_Module_47",
+        "pdf": "47_Climatology_-_Module_47/Climatology-Module47-1540213455.pdf",
+        "spotify_link": "https://open.spotify.com/episode/634Qvo7fgJ1ATJhh9CAJpI?si=MvUHPDzARd-HY9ZKoAyGlA"
+    },
+    {
+        "name": "48_Climatology_-_Module_48",
+        "pdf": "48_Climatology_-_Module_48/Climatology-Module48-1540389119.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3tOc3Q8EQ7cPBRUMPOLD1q?si=dKQbC7--SJWAzl7em1AkRA"
+    }
+];
