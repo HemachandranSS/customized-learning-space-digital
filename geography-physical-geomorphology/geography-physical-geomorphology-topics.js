1889090b9378fb2f9151aa8284df9596cdf384e1
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 13:37:37 +0530

    Added Geography: Physical and Geomorphology

---
+++ geography-physical-geomorphology/geography-physical-geomorphology-topics.js
@@ -0,0 +1,197 @@
+const topics = [
+    {
+        "name": "1_Physical Geography -Module-1",
+        "pdf": "1_Geomorphology_-_Module_-1/PhysicalGeography-Module-1-1511613623.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "2_Physical Geography -Module2",
+        "pdf": "2_Geomorphology_-_Module_-2/PhysicalGeography-Module2-1512377723.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "3_Physical Geography -Module3",
+        "pdf": "3_Geomorphology_-_Module_-_3/PhysicalGeography-Module3-1512377764.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "4_PhysicalGeography-Module-4",
+        "pdf": "4_Geomorphology_-_Module_-_4/PhysicalGeography-Module-4-1512222869.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "5_Physical Geography Module - 5",
+        "pdf": "5_Geomorphology_-_Module_-_5/PhysicalGeographyModule5-1512826091.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "6_Physical Geography Module - 6",
+        "pdf": "6_Geomorphology_-_Module_-_6/PhysicalGeographyModule6-1512829304.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "7_Physical Geography Module - 7",
+        "pdf": "7_Geomorphology_-_Module_-_7/GEOGRAPHYLECTURE-7-FINAL-PDF-1515562565.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "8_Physical Geography Module - 8",
+        "pdf": "8_Geomorphology_-_Module_-_8/PhysicalGeography-Module8-1514034834.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "9_Physical Geography Module - 9",
+        "pdf": "9_Geomorphology_-_Module_-_9/PhysicalGeography-Module9-1514035229.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "10_Physical Geography Module - 10",
+        "pdf": "10_Geomorphology_-_Module_-_10/PhysicalGeography-Module10-1514036018.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "11_Physical Geography Module - 11",
+        "pdf": "11_Geomorphology_-_Module_-_11/GEOGRAPHYLECTURE-11-F-pdf-1515249413.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "12_Physical Geography Module - 12",
+        "pdf": "12_Geomorphology_-_Module_-_12/GEOGRAPHYLECTURE-12-F-pdf-1515249499.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "13_Physical Geography Module - 13",
+        "pdf": "13_Geomorphology_-_Module_-_13/PhysicalGeography-Module13-1517670263.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "14_Physical Geography Module - 14",
+        "pdf": "14_Geomorphology_-_Module_-_14/Geomorphology - Module 14.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "15_Physical Geography Module - 15",
+        "pdf": "15_Geomorphology_-_Module_-_15/PhysicalGeography-Module15-1518873113.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "16_Physical Geography Module - 16",
+        "pdf": "16_Geomorphology_-_Module_-_16/PhysicalGeography-Module16-1518874714.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "17_Geomorphology_-_Module_17",
+        "pdf": "17_Geomorphology_-_Module_17/Geomorphology-Module17-1524837185.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "18_Geomorphology_-_Module_18",
+        "pdf": "18_Geomorphology_-_Module_18/Geomorphology-18-1529653107.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "19_Geomorphology_-_Module_19",
+        "pdf": "19_Geomorphology_-_Module_19/Geomorphology-Module19-1529656016.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "20_Geomorphology_-_Module_20",
+        "pdf": "20_Geomorphology_-_Module_20/Geomorphology-Module20-1530021817.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "21_Geomorphology_-_Module_21",
+        "pdf": "21_Geomorphology_-_Module_21/Geomorphology-Module21-1530100919.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "22_Geomorphology_-_Module_22",
+        "pdf": "22_Geomorphology_-_Module_22/Geomorphology-Module22-1530101105.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "23_Geomorphology_-_Module_23",
+        "pdf": "23_Geomorphology_-_Module_23/Geomorphology-Module23-1531747999.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "24_Geomorphology_-_Module_24",
+        "pdf": "24_Geomorphology_-_Module_24/Geomorphology-Module24-1531748242.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "25_Geomorphology_-_Module_25",
+        "pdf": "25_Geomorphology_-_Module_25/Geomorphology-Module25-1531822794.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "26_Geomorphology_-_Module_26",
+        "pdf": "26_Geomorphology_-_Module_26/Geomorphology-Module26-1532095854.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "27_Geomorphology_-_Module_27",
+        "pdf": "27_Geomorphology_-_Module_27/Geomorphology-Module27-1532435938.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "28_Geomorphology_-_Module_28",
+        "pdf": "28_Geomorphology_-_Module_28/Geomorphology-Module28-1532436032.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "29_Geomorphology_-_Module_29",
+        "pdf": "29_Geomorphology_-_Module_29/Geomorphology-Module29-1533212030.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "30_Geomorphology_-_Module_30",
+        "pdf": "30_Geomorphology_-_Module_30/Geomorphology-Module30-1533213714.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "31_Geomorphology_-_Module_31",
+        "pdf": "31_Geomorphology_-_Module_31/Geomorphology-Module31-1533561910.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "32_Geomorphology_-_Module_32",
+        "pdf": "32_Geomorphology_-_Module_32/Geomorphology-Module32-1533735547.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "33_Geomorphology_-_Module_33",
+        "pdf": "33_Geomorphology_-_Module_33/Geomorphology-Module33-1533820494.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "34_Geomorphology_-_Module_34",
+        "pdf": "34_Geomorphology_-_Module_34/Geomorphology-Module34-1534423874.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "35_Geomorphology_-_Module_35",
+        "pdf": "35_Geomorphology_-_Module_35/Geomorphology-Module35-1534765911.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "36_Geomorphology_-_Module_36",
+        "pdf": "36_Geomorphology_-_Module_36/Geomorphology-Module36-1534935763.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "37_Geomorphology_-_Module_37",
+        "pdf": "37_Geomorphology_-_Module_37/Geomorphology-Module37-1535014570.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "38_Geomorphology_-_Module_38",
+        "pdf": "38_Geomorphology_-_Module_38/Presentation1-1536146670.pdf",
+        "spotify_link": ""
+    },
+    {
+        "name": "39_Geomorphology_-_Module_39",
+        "pdf": "39_Geomorphology_-_Module_39/Presentation1-1536146761.pdf",
+        "spotify_link": ""
+    }
+];
