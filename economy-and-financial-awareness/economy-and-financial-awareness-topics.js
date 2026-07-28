71693e1eb2fa2657d05904bb85c53ae22d86e8a9
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 18:30:00 +0530

    Added Jul 28, 2026 Finacial Awareness'

---
+++ economy-and-financial-awareness/economy-and-financial-awareness-topics.js
@@ -0,0 +1,52 @@
+const topics = [
+    {
+        "name": "Economy Financial Awareness - Module 1",
+        "pdf": "EconomyFinancialAwareness-Module1-1543657043.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4m0CTLjijWnwqqMsHbEPqM?si=vlSL3WTDRNuuQUsxkljNLw"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 2",
+        "pdf": "Presentation1-1544857639.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6FnGUNHWxU2RsnSGv0X4If?si=w440vwJCSbO71JLBwIEvjw"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 3",
+        "pdf": "ECONOMYFINANCIALAWARENESS-3-1544858922.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0zyF7dfQa1qXZtSNJMeXvj?si=gADzdvAHRO6RuY5ytAAZGA"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 4",
+        "pdf": "EconomyFinancialAwareness-Module4-1545652643.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3XtGwAYNa1r0EGBHvuyDxO?si=7ASOxiAxSO2CjOHA5xIcwQ"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 5",
+        "pdf": "FinancialAwareness-Module-5-1546683334.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1yX3xzYtfyhPn2HsSdUqNe?si=y6IW7tHJRp2kyUVeL-NjXA"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 6",
+        "pdf": "FinancialAwareness-6-1547279769.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3cXYWx7SAEMRjiSJaafSUd?si=aEBxT3OIR02kQ4r98Dghpg"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 7",
+        "pdf": "EconomyFinancialAwareness-Module7-1547894529.pdf",
+        "spotify_link": "https://open.spotify.com/episode/61YUhEbseXDg8kTUOwwYQZ?si=SZ08buw7QGSw0FZyjis5rQ"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 8",
+        "pdf": "EconomyFinancialAwareness-Module8-1548420578.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3vRBkS3hwaAhh6iuPzK0O4?si=HDERVoUjRTSqbjVwuqRdRQ"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 9",
+        "pdf": "EconomyFinancialAwareness-Module9-1549112278.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6siwBO9zHTW9R4FHutq1Hv?si=bjTrZfQ6Sj6ZpJKAUKYvkg"
+    },
+    {
+        "name": "Economy Financial Awareness - Module 10",
+        "pdf": "EconomyFinancialAwareness-Module10-1549697301.pdf",
+        "spotify_link": "https://open.spotify.com/episode/42FQxEN0Nm51XNNazLKJLe?si=-ptRx6xYR72WVp2h1Ez2lA"
+    }
+];
