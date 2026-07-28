ab9e7f78c7d90704e42ab46f593b550d22883076
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 09:36:57 +0530

    Jul 28 , 2026

---
+++ post-independent-development/post-independent-development-topic.js
@@ -0,0 +1,107 @@
+const topics = [
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 1.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5TQZSfz78zQ5kAIbeSFExO?si=3zs2G3gYSqiF0eTFXB1QoQ"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 2.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/58kchdz8INwRRTfcv5IqNX?si=wIVWqlUNScyBLCeUSqFqQg"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 3.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/329utSMoFFW7jnVavpgsvo?si=ZNzCjbDhRuST2maQcn9_dg"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 4.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5zYdlDq6nipU0yTMGp4CPR?si=GsSYFeTWRTyBHuCHInySQw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 5.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7qg6sqCnQcLOBqyPd0laux?si=RwEhIeMcRSCW9zIO0VZBHw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 6.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5NuukEonUiLDwEVk2UI6ir?si=VTVRgId7TO6IRpX6AkDr3Q"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 7.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0wFiJOaq7kBV0lDybAwois?si=lbkW4S20QTG9oL1a71XWLw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 8.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/78NcnN6uQMf1AGyZLQergC?si=pR59iLBPTfWqRmpDV14H4w"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 9.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1SJQoorVfHh7LBw9Aog9L9?si=TKdFDuKsSratH3OnfVIeFw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 10.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3S6HxP6ztVZN39jquqBNH0?si=EnBZ-xvuSZSjzek8FuThvA"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 11.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1B10luZk4Wd0Z84Y0OJfwk?si=BLtEnvGfRbeRhwaNYu0S5Q"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 12.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3yRCVn6RziRagV7BPFoCwW?si=tY4MnvHGRB2XSMWm_y_n5g"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 13.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6UiHXvshsTptNpLROVNTzQ?si=0cCWkgzoQf2aQV9EfAvrWw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 14.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/063mA2QNmbRGVZZlcb6IDU?si=VItO3nAcTkKvnTmJfviStw"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 15.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0BtXdPnQHQ4vkmhhvpmAoC?si=DNG20H-NS_ujq3tL3PUmmg"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 16.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3OtsnLxd5AKwbh9MjSdwLI?si=T4sWHbTBSRWvfUSMdnuipA"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 17.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6grJTH8kmJFAJV1e9i7TAk?si=gQZNh3MdTHa-su6jxbqOgg"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 18.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7dUgNBTHbEBZlgdLMWm7Rp?si=-3w8KjegTYKOASmd9osgpQ"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 19.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5cDmEvvwbMLhTdC2zCfyd7?si=RzmJjsYYSXqgLIbPjcjVCQ"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 20.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1yYT3lMHNaIa2LUldRdVd3?si=NuaWee1WQou-lAPKbu200A"
+    },
+    {
+        "name": "Post Independence Developments (Revised & New) - Module - 21.pdf",
+        "pdf": "Post Independence Developments (Revised & New) - Module - 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1GaZ3D5PZybugYVsz9dC5k?si=uEvc7QQ4TiayRfJ5AYCFGA"
+    }
+];
