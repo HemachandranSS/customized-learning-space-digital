c6d6bb7cab96a2cac0149b41e7bd6511b388d495
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 16:51:35 +0530

    Added Standard GK

---
+++ standard-gk-india-the-world/standard-gk-india-the-world-topics.js
@@ -0,0 +1,122 @@
+const topics = [
+    {
+        "name": "Standard GK (India & The World) - Module 1",
+        "pdf": "Standard GK (India & The World) - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6FNEnRKZWSRBuZY6CigSPn?si=X2WaH92TRZmtZ1NPWkqRAA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 2",
+        "pdf": "Standard GK (India & The World) - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/43GnKGR35rMgS4ikUvtlQV?si=osnzddueT_yi91kIISuviA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 3",
+        "pdf": "Standard GK (India & The World) - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3QTJmeefh0A00jKlkJcooN?si=-ZDa4zvCSEaXLbLTwosriw"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 4",
+        "pdf": "Standard GK (India & The World) - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6aVL9SLIHBfj6LVYVJrwnU?si=Ff9r4MafQ5mYKT8gOCgd6A"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 5",
+        "pdf": "Standard GK (India & The World) - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7nWezrcuwNEdd8rlY1oLyY?si=x-z5oiZnSfKGRuOYCDCogA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 6",
+        "pdf": "Standard GK (India & The World) - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5KtppgTqGVM1SbXwZWovYq?si=Kah9I6haROGVC4xgxR5_Eg"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 7",
+        "pdf": "Standard GK (India & The World) - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3hlcvvJKyaXCZr9xLgei5A?si=pTic5VGJTy2yPyV8SCN7EA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 8",
+        "pdf": "Standard GK (India & The World) - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0MDgrgOdiPuiSxQAaHoWuh?si=5tUNLBJtS22YrX7mIJTtGA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 9",
+        "pdf": "Standard GK (India & The World) - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0vJAMaAo5TxyXzlCZuIv1v?si=y4-OjUu_RXSSmYFqkVw3nA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 10",
+        "pdf": "Standard GK (India & The World) - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0H9p5ohMhOkNfAEtHQoop5?si=BUGQS3rZRPSqAQr27pvJYA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 11",
+        "pdf": "Standard GK (India & The World) - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5neSV4WXDhGKqSHmBt7acJ?si=lceBpXbOTw2kOTny9ZkwOA"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 12",
+        "pdf": "Standard GK (India & The World) - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0i81hyeabBDEcWFgNjDAIB?si=D4Yk0CamSiWQi55ctU4-9Q"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 13",
+        "pdf": "Standard GK (India & The World) - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/259QZnFbLE4GTE6BEiATqR?si=E7SmLJNbRLet7UcF0BmP7Q"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 14",
+        "pdf": "Standard GK (India & The World) - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4BTiPiXY8sDDamDmCJSDGT?si=xqv9s86JTpy2WSqxHwaOHg"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 15",
+        "pdf": "Standard GK (India & The World) - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1Hu0YMn7Nhk06huULZ6tIh?si=FzhYFPLKScySoMn9vJTqGg"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 16",
+        "pdf": "Standard GK (India & The World) - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4bFMmkxCkGNuz62sg9iSjD?si=m-d4YgZxQKqqgtkhN5bKDQ"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 17",
+        "pdf": "Standard GK (India & The World) - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4YplvHxcvulegauybPxKz6?si=0-ZPJ6znR-OEv4bJv9Qt6A"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 18",
+        "pdf": "Standard GK (India & The World) - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7yV5SvwEcxaAROnXwTEB0I?si=WBuPOyURTKin49nK_zbmcg"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 19",
+        "pdf": "Standard GK (India & The World) - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/47A7vliSeEMh6ehh66kNFE?si=gwBI_QixToW97Dh3UV4RKw"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 20",
+        "pdf": "Standard GK (India & The World) - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6UTr8UxTPXMrOyHhXkPKR0?si=BhR0Su_ARl2hhoP-fK6D1Q"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 21",
+        "pdf": "Standard GK (India & The World) - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6GzVNDKlbAmFtM7LS3ueNa?si=8LdN3OSIQeC25DJiRPREwQ"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 22",
+        "pdf": "Standard GK (India & The World) - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1HivJcGCLYkwLsz9qreJOo?si=_sFAyt-wSly_7a4IAODrIg"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 23",
+        "pdf": "Standard GK (India & The World) - Module 23.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0CLYelSZfg0rQd4nROl1JK?si=pLizr9uDRw6fpD_8-lccXQ"
+    },
+    {
+        "name": "Standard GK (India & The World) - Module 24",
+        "pdf": "Standard GK (India & The World) - Module 24.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7Fb0iP0DInz3VqIpK2e8qI?si=xk7wja5TSge8is7-OfNYNg"
+    }
+];
