3ca18fb55410ffc4da0bc934e5a73364d63978c3
Hemachandran SS <hemachandran@colanonline.com>
2026-07-28 10:03:08 +0530

    Jul 28 , 2026 - Added Art and Culture

---
+++ india-art-and-culture/india-art-and-culture-topic.js
@@ -0,0 +1,362 @@
+const topics = [
+    {
+        "name": "Art and Culture - Module 1",
+        "pdf": "01-art-and-culture/Art and Culture - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2eWO6IJYjKP8cVSEnJNXhM?si=ycKVy0rRSV-EF_Xx-H0v2g"
+    },
+    {
+        "name": "Art and Culture - Module 2",
+        "pdf": "01-art-and-culture/Art and Culture - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5CDTEfpR1Li58SWjc7RAzh?si=ZwjwvVF3RuO7LtMx-tss3Q"
+    },
+    {
+        "name": "Art and Culture - Module 3",
+        "pdf": "01-art-and-culture/Art and Culture - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6zA1otGGbQUn4GTuqtcdBA?si=n0eZT649SdO0Y4NcwxPE8Q"
+    },
+    {
+        "name": "Art and Culture - Module 4",
+        "pdf": "01-art-and-culture/Art and Culture - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3wNTu90E96UF23dACMHhUK?si=TxfGrbFKRamW0vWoggiq-Q"
+    },
+    {
+        "name": "Art and Culture - Module 5",
+        "pdf": "01-art-and-culture/Art and Culture - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7lij1002b6EQ9R9sVPhcQf?si=sLnNyaRBTk2CudVkXX7AZQ"
+    },
+    {
+        "name": "Art and Culture - Module 6",
+        "pdf": "01-art-and-culture/Art and Culture - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6HQglJJ4z7hYwaIxmLlPWL?si=DAhwbm3PQW24a0XtQfLzHA"
+    },
+    {
+        "name": "Art and Culture - Module 7",
+        "pdf": "01-art-and-culture/Art and Culture - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5r8InyS7vnWP58O4hRY8AL?si=YB4BtDFHSva3kfCMit-EWw"
+    },
+    {
+        "name": "Art and Culture - Module 8",
+        "pdf": "01-art-and-culture/Art and Culture - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7INhAlFqjEjGgggGlf007h?si=hvSNnRP_RdOstOdl9SS-Eg"
+    },
+    {
+        "name": "Art and Culture - Module 9",
+        "pdf": "01-art-and-culture/Art and Culture - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2hHU00q0pAUmC27W00iYwX?si=bVVNkygnRLOVF33nitx4Rg"
+    },
+    {
+        "name": "Art and Culture - Module 10",
+        "pdf": "01-art-and-culture/Art and Culture - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6i8hFFvQyJHb4KRT3sNJ46?si=OaWTVBWlQNaJSnANOgYlIg"
+    },
+    {
+        "name": "Art and Culture - Module 11",
+        "pdf": "01-art-and-culture/Art and Culture - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6XitYiAXh1i7FnG8q1jqsD?si=Q0JZjSHzTVexV8kqtJkjVA"
+    },
+    {
+        "name": "Art and Culture - Module 12",
+        "pdf": "01-art-and-culture/Art and Culture - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7dfck2MZdzDlSgjJSq4Omw?si=tm2JJNn1TxeTZ5hcFQq_Pg"
+    },
+    {
+        "name": "Art and Culture - Module 13",
+        "pdf": "01-art-and-culture/Art and Culture - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0xN8Bl7vL4KdvxgbNHgJvG?si=sJ0qJtUIQnemOdhv4fnVJA"
+    },
+    {
+        "name": "Art and Culture - Module 14",
+        "pdf": "01-art-and-culture/Art and Culture - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5cvGsRHIusqUSHLb3614rD?si=udz5hOapS2KgA31Dgw_ZNw"
+    },
+    {
+        "name": "Art and Culture - Module 15",
+        "pdf": "01-art-and-culture/Art and Culture - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2RXqW60eaW8s2E3HtMbH2J?si=N-jh12-WRyu4Q1daE4N41A"
+    },
+    {
+        "name": "Art and Culture - Module 16",
+        "pdf": "01-art-and-culture/Art and Culture - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5zkDNY7ZIIkNypbTPAJmLP?si=xE7AXj7WRmiaaNwx8t_YUQ"
+    },
+    {
+        "name": "Art and Culture - Module 17",
+        "pdf": "01-art-and-culture/Art and Culture - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7GltCugFqkoJsmDaNCCXbS?si=9P_yPzFPR6CSODIIMJd6iQ"
+    },
+    {
+        "name": "Art and Culture - Module 18",
+        "pdf": "01-art-and-culture/Art and Culture - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6Gps2pyZwGvxtEHEdhp0Mw?si=wOtLZZdlQEedJf4C8toqkg"
+    },
+    {
+        "name": "Art and Culture - Module 19",
+        "pdf": "01-art-and-culture/Art and Culture - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5DXMX6xdULlHY51qJVqw3B?si=8GJYCcB6SPOUzjOOEUGchQ"
+    },
+    {
+        "name": "Art and Culture - Module 20",
+        "pdf": "01-art-and-culture/Art and Culture - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2YjVlCocVUCPdty7PCzZH3?si=YmbB6_YiRwGAUo8YJ3sEbw"
+    },
+    {
+        "name": "Art and Culture - Module 21",
+        "pdf": "01-art-and-culture/Art and Culture - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1v1YmuttMaNBVjRkv5xHjJ?si=enfl0vYoSxicg__B6Ecaig"
+    },
+    {
+        "name": "Art and Culture - Module 22",
+        "pdf": "01-art-and-culture/Art and Culture - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3VRfXmSiRkxZYItoQQkews?si=z0mPOGGbSZuKyqMoYpKu9A"
+    },
+    {
+        "name": "Art and Culture - Module 23",
+        "pdf": "01-art-and-culture/Art and Culture - Module 23.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7adbHqSox1WnXBKuJ5DF4v?si=DqXNpS98S9eAlZzk72hLwg"
+    },
+    {
+        "name": "Art and Culture - Module 24",
+        "pdf": "01-art-and-culture/Art and Culture - Module 24.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4JqDLmNOQk1RWgexLer8yv?si=pNy49OBIQM-cXUv1iYP5ZA"
+    },
+    {
+        "name": "Art and Culture - Module 25",
+        "pdf": "01-art-and-culture/Art and Culture - Module 25.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1GWP9oSqbmA0Gi9IOBlQmn?si=XZm01dEbSEusammfhu59Ng"
+    },
+    {
+        "name": "Cultural Institutions and Awards -Module 1",
+        "pdf": "02-cultural_institutions-and-awards/Cultural Institutions and Awards -Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0Xljma4j96EG2ELBENKGxN?si=ve-o206mSFuEzCTsKfyAaw"
+    },
+    {
+        "name": "Cultural Institutions and Awards -Module 2",
+        "pdf": "02-cultural_institutions-and-awards/Cultural Institutions and Awards -Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2X0hMRU9j8KIZP6JMRvioo?si=SHNkqtiqQDOfAqH15_mTUg"
+    },
+    {
+        "name": "Cultural Institutions and Awards -Module 3",
+        "pdf": "02-cultural_institutions-and-awards/Cultural Institutions and Awards -Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4ynWe9ORYA7i9z9ksiiHyO?si=-R-zdVbiQSKLYw2A_cpxKA"
+    },
+    {
+        "name": "Cultural Institutions and Awards -Module 4",
+        "pdf": "02-cultural_institutions-and-awards/Cultural Institutions and Awards -Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0ogT5jxbrzqYwDgVjl1tyj?si=N5L90tggTIKOdHYA9XcztA"
+    },
+    {
+        "name": "Indian Paintings - Module 1",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3N4wJVqZeg29Hr8Td3j4Py?si=6bY3IO7vQRmJLdm22Ow8oA"
+    },
+    {
+        "name": "Indian Paintings - Module 2",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3Slz0gUHSwNogTMC2oHMYn?si=L5Lidk6wS8GQsWFeUaG03Q"
+    },
+    {
+        "name": "Indian Paintings - Module 3",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7sTc29i0lWCYPmah5HTibJ?si=fQ8ML0KMTyGJEFtc5KeEJw"
+    },
+    {
+        "name": "Indian Paintings - Module 4",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4Ai518Xmxx34SRU58pvuPh?si=c_rF2qsvRCqw3bgc0-U36g"
+    },
+    {
+        "name": "Indian Paintings - Module 5",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5DRZb75BPnj6rEay2Njoc6?si=Hnzsx8-pSeanZPi80_MAdg"
+    },
+    {
+        "name": "Indian Paintings - Module 6",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3gICpCgGZiygXDKGsFyUuj?si=L2C-bzQcQdWC0pwNOEDIwA"
+    },
+    {
+        "name": "Indian Paintings - Module 7",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2wH0jpSuM3PdY0Dc88F47g?si=xhYuLbriRyCmMwlGei5M2A"
+    },
+    {
+        "name": "Indian Paintings - Module 8",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3e8hvUkQBySCPzKkmtLx9L?si=Y7EsvO9_TIGAo8bETyOxqA"
+    },
+    {
+        "name": "Indian Paintings - Module 9",
+        "pdf": "03-indian-paintings/Indian Paintings - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5ehjPzm3myU4ByHVD8Un8r?si=A5cwNWlNSn2VrKTcOAwSeQ"
+    },
+    {
+        "name": "Language and Literature - Module 1",
+        "pdf": "04-language-and-literature/Language and Literature - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/671hwiBu6DQRSBG8A4dFgJ?si=m5jUahtVQP6vryHUTv9RVw"
+    },
+    {
+        "name": "Language and Literature - Module 2",
+        "pdf": "04-language-and-literature/Language and Literature - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6GJHymbhwCfgmWlKobL3JX?si=Bc0pBRLlQlyhxQ1TnyQQJQ"
+    },
+    {
+        "name": "Language and Literature - Module 3",
+        "pdf": "04-language-and-literature/Language and Literature - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0fqqM7g7Hh0kjczdN5a8lD?si=pvpq76YGS8uCDTYxU4V33Q"
+    },
+    {
+        "name": "Language and Literature - Module 4",
+        "pdf": "04-language-and-literature/Language and Literature - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/19QRPHA4lNFitfpfvsremo?si=2fvRwKg9QoanSWjCPYixJg"
+    },
+    {
+        "name": "Language and Literature - Module 5",
+        "pdf": "04-language-and-literature/Language and Literature - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/33iOMbcW7ECjq9LoVyXz8f?si=AavYvi_ZQM6-VJoGq6r7Tw"
+    },
+    {
+        "name": "Language and Literature - Module 6",
+        "pdf": "04-language-and-literature/Language and Literature - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5E7YdRhssAmpErCtOVBX7w?si=3A--ShV6SH2DGb95UE9C_g"
+    },
+    {
+        "name": "Language and Literature - Module 7",
+        "pdf": "04-language-and-literature/Language and Literature - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6QMM2lfO8KER4vQRxdnkQs?si=bT9i_rpDQHysGhyy0uksnw"
+    },
+    {
+        "name": "Puppetry & Martial Arts - Module 1",
+        "pdf": "05-puppetry-and-martial-arts/Puppetry & Martial Arts - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/36x0G1rbVAdIGDAmZBGqKa?si=LAWmIA7AR02CpFCXC-0ASQ"
+    },
+    {
+        "name": "Puppetry & Martial Arts - Module 2",
+        "pdf": "05-puppetry-and-martial-arts/Puppetry & Martial Arts - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5Zc4F9ZjNCTZcazfJRhVD2?si=ek7drz6uSYSp---EoE4IsA"
+    },
+    {
+        "name": "Puppetry & Martial Arts - Module 3",
+        "pdf": "05-puppetry-and-martial-arts/Puppetry & Martial Arts - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2HQ5wdUOaxPTWrVgxq7sw7?si=osmsCDEvQDWDbEPjEZk_xA"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 1",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3yUZ7CTnZriXrDfDOZ7QAo?si=ayS22VQ1QBuFDnkfEUkp2g"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 2",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2ujAgmDpUeMQIoSR4i1pBR?si=Q8tUhFlPQ-yjpp97ocCCBg"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 3",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4XYsepEXBzDPJprr0KG43K?si=QgMq2ivaRW6CVIYbjORI9Q"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 4",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0YJ0W2G3RGBOi2eV6dxbX1?si=EaRAlUDLTUG4kg8trwhx4A"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 5",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0bVhhHKMc3Z3dz7coavpFz?si=hRKRAfXUTgaP_IYJ0NeDhg"
+    },
+    {
+        "name": "Religion and Philosophy - Module - 6",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module - 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6iwWdAkV9pYVC36L9DlKTT?si=dblfegA3S0yANdwWfuzjTw"
+    },
+    {
+        "name": "Religion and Philosophy - Module 7",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5mP44v9S6hB25aPvLQyw8x?si=6fGdYNX6SCOAcyOholDM3w"
+    },
+    {
+        "name": "Religion and Philosophy - Module 8",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1gLpQVLalMzZEWcQNkHyGL?si=GBcNQjOARCuHSjiy73SGfQ"
+    },
+    {
+        "name": "Religion and Philosophy - Module 9",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2ZpOPns7T1tGFXEG2sC9N6?si=WDp12CvITYWdU8WxKy1rlA"
+    },
+    {
+        "name": "Religion and Philosophy - Module 10",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4lljo9v2PazoyhNiQ03o9g?si=F299C4-4Qz2SVPH0XjX-OQ"
+    },
+    {
+        "name": "Religion and Philosophy - Module 11",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3wg51YN2LsFX3qrszx8PAo?si=pw35NbBRTv-AtI7FwwzyOw"
+    },
+    {
+        "name": "Religion and Philosophy - Module 12",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7esqPtUkZPs6PCDf8Xa27G?si=gxYLwQcTSQuUkEeXpqLyxQ"
+    },
+    {
+        "name": "Religion and Philosophy - Module 13",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/592XXqsXyU2Kn4Byq4OGrd?si=SZ9yn-6ZSt-SIhokjzPoWg"
+    },
+    {
+        "name": "Religion and Philosophy - Module 14",
+        "pdf": "06-religion-and-philosophy/Religion and Philosophy - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4J3mI32SnAJJhhhnwHeK4w?si=zDVfkPXDR3CnYgS8GokuxA"
+    },
+    {
+        "name": "Indian Dance - Module 1",
+        "pdf": "07-indian-dance/Indian Dance - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6H9yjhXA0tQXLUA6v6mZ2w?si=IbH7SPUNTRuQXiv6lFQXBg"
+    },
+    {
+        "name": "Indian Dance - Module 2",
+        "pdf": "07-indian-dance/Indian Dance - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7mc6kIMkzEdDmmiHFb0W39?si=outfuErsR9aUyYj0Ern8lQ"
+    },
+    {
+        "name": "Indian Dance - Module 3",
+        "pdf": "07-indian-dance/Indian Dance - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1dq0hrgOQt4isBwD7bqlx6?si=FMRXfZbUSzqDi0SmtiA-kw"
+    },
+    {
+        "name": "Indian Dance - Module 4",
+        "pdf": "07-indian-dance/Indian Dance - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1a4haZLYNw2cpngKGraRIq?si=4ZVf3ooYQietBt69tFGYjQ"
+    },
+    {
+        "name": "Indian Music - Module 1",
+        "pdf": "07-indian-dance/Indian Music - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6R1wkyHe9QAaAcZo6GaCOk?si=rD2uwsEuSLOzLekIxGQFCQ"
+    },
+    {
+        "name": "Indian Music - Module 2",
+        "pdf": "07-indian-dance/Indian Music - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2jvVzd8sn0V4G2SJo7CkV4?si=9IzuDVMPSwC57KX9V4tLag"
+    },
+    {
+        "name": "Indian Theatre - Module 1",
+        "pdf": "08-indian-theatre/Indian Theatre - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1LreePJ8vMogVklhbOk74Y?si=Nu4h14zhT3WjFUFyTdPreA"
+    },
+    {
+        "name": "Indian Theatre - Module 2",
+        "pdf": "08-indian-theatre/Indian Theatre - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5iWjeLjVnZgW0xwjBJdK3N?si=OdBUkbtBT9OmfSxp5tZ_Hg"
+    },
+    {
+        "name": "Indian Theatre - Module 3",
+        "pdf": "08-indian-theatre/Indian Theatre - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5v728LFDfOUZHvcJ5s4Ggb?si=T4ctk_UXQnWzmOvikJI7Uw"
+    },
+    {
+        "name": "Indian Theatre - Module 4",
+        "pdf": "08-indian-theatre/Indian Theatre - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5JKnCcKjQ6W84qOqOb3dor?si=t7-tvbMiSvSF6NvL12LY4w"
+    }
+];
