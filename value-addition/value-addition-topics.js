1533aa1aefcc3ed8379a13a8ed3d600a6a32a732
Hemachandran SS <hemachandran@colanonline.com>
2026-07-30 19:35:32 +0530

    Added Value Addition Modules

---
+++ value-addition/value-addition-topics.js
@@ -0,0 +1,87 @@
+const topics = [
+    {
+        "name": "UPSC Mains - Value Addition - Module 1",
+        "pdf": "UPSC Mains - Value Addition - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0DHQ3smIu6sFYbzirTxPZU?si=1fEEPRgRTzysAyx4Zrs6ww"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 2",
+        "pdf": "UPSC Mains - Value Addition - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4fGVyzPpcCheE4K6Fl4kPx?si=Bf4vI8YvSA-B7542xJOoUQ"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 3",
+        "pdf": "UPSC Mains - Value Addition - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2QRnSCTylQqjdXHG36YyGf?si=ltCXTimVT6ivRgK7dJfIeg"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 4",
+        "pdf": "UPSC Mains - Value Addition - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5iYw04tXFZo9gx27ZhlskU?si=bVYdGIMNR62jkDjVJzRFIA"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 5",
+        "pdf": "UPSC Mains - Value Addition - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1HhcoklCpHnCYRTTzwFgd3?si=dHhNirbdTjOQC90OH_PszA"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 6",
+        "pdf": "UPSC Mains - Value Addition - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6LdXdiSKBREUQ83lEakWLv?si=r2VOIkidTaOPZnsh1WqW-g"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 7",
+        "pdf": "UPSC Mains - Value Addition - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/22IivR9333vIoa0jUtTGzv?si=DVAKN6PiRe6B77WhCgX3wQ"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 8",
+        "pdf": "UPSC Mains - Value Addition - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2Yd1nWn3DOM9x0dMsFQBa4?si=zw9HiMdmTEGqLcCywNED2w"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 9",
+        "pdf": "UPSC Mains - Value Addition - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7gpBvwJCWFU1QoYmmelPSQ?si=_AdiPo0TQ5OHhT9-gfYt0g"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 10",
+        "pdf": "UPSC Mains - Value Addition - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1aRFaqJUo2ZJwVsoFSHW4G?si=vZuXquWKTU2w_pDEDc4G5A"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 11",
+        "pdf": "UPSC Mains - Value Addition - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4qxJTFpmbm8gjBouafJgEx?si=pi7dr0HKSj6JHxZhOu27Iw"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 12",
+        "pdf": "UPSC Mains - Value Addition - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/47yytwmYu5HnhFxhriGM99?si=8xqc6b9JTb-ByTQYB1iIVQ"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 13",
+        "pdf": "UPSC Mains - Value Addition - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/34Cn32VUIrNBHhnAaY28hL?si=nMunrBObSDGepKMEUe3E-Q"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 14",
+        "pdf": "UPSC Mains - Value Addition - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/4eZdnRPVf62qJ7Cd7IjEht?si=rFzH34zkQEqg3ubNH0aa-Q"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 15",
+        "pdf": "UPSC Mains - Value Addition - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5rd0GFhYfurFozw8oLyUFP?si=j7NQL0hNTwi58knwZUrd2A"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 16",
+        "pdf": "UPSC Mains - Value Addition - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/628cZSawlhMoHumAxK03m2?si=XR-CLMynTHmPkh7bCxgoKg"
+    },
+    {
+        "name": "UPSC Mains - Value Addition - Module 17",
+        "pdf": "UPSC Mains - Value Addition - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2aTFAEI4UKSYGxYS5wKKWx?si=SDY1QztvRpK8qkpy0PmQDQ"
+    }
+];
