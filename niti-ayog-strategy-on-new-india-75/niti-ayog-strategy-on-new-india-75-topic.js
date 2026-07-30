03a1bf7ef81fc427da3066f1dbabd26a96a15b78
Hemachandran SS <hemachandran@colanonline.com>
2026-07-30 18:37:26 +0530

    Jul 30, 2026 - Added NITI Ayog Strategy for 75th Independence Day

---
+++ niti-ayog-strategy-on-new-india-75/niti-ayog-strategy-on-new-india-75-topic.js
@@ -0,0 +1,112 @@
+const topics = [
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 1",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 1.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2scmE60BDMTD357n7mD1yq?si=Waz7C1KYQEedH8LISJoGlg"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 2",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 2.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6mle3kplnEX9T8Pp8qdfrp?si=RiGF2BjnQKy3bCO3DtrusA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 3",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 3.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7hzHG8gsTnYwMcRlWSUGAU?si=sFYa67vpS5qsASNGvLJLZw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 4",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 4.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7rqttPOuQOjWTI9cKnEooi?si=66aqK_YeRNiZZcb6LtM4Xg"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 5",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 5.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2NeZUU6fTdBDtCQh5IZSxt?si=ppUFxqOCQMmx6nn4fvrZJA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 6",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 6.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3OGzgjAL1qp0lb85wUSwYB?si=cYEykOVMRsqS4qVMNlagIQ"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 7",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 7.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3uBUb3rr84TytvxVgoJNzs?si=rZkYdWglREuCcPwVm1okaQ"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 8",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 8.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3f08Qt03EKZATj1pSnVuok?si=MoExF_wmS5a3ZuY5XCUWfw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 9",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 9.pdf",
+        "spotify_link": "https://open.spotify.com/episode/0TlaXt703kLYExzsKMjDc2?si=5uhKZDhMQP6FdLl3x84bFA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 10",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 10.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7uX6hORjYZPO07DHAYSl9M?si=BHzrOBwKTF2vKly4nSg2sA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 11",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 11.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6HcLhHLoukznl9b1Mwo1JN?si=arhzCaLyTXScv0B7GKY4aQ"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 12",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 12.pdf",
+        "spotify_link": "https://open.spotify.com/episode/2Uj95gQIt3UX4ilZ274Zq6?si=m8uvdkgLQc6bR-ze8Ku3Yw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 13",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 13.pdf",
+        "spotify_link": "https://open.spotify.com/episode/5mPVm6vtumUdpdrV4FGilB?si=9a3g6u-fRkKXgJT_MbDxoA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 14",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 14.pdf",
+        "spotify_link": "https://open.spotify.com/episode/54uEunJaTSGsWI0U7CjhjP?si=e0_F9TsaSTSea8orKvUXBw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 15",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 15.pdf",
+        "spotify_link": "https://open.spotify.com/episode/6MBMYq2X54heqC4RNgr7NC?si=cy8JEzgPRf-Z2LRZOuhXDQ"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 16",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 16.pdf",
+        "spotify_link": "https://open.spotify.com/episode/55qbyxGa4iWT4yeCdvHHDr?si=tCo9N35CRj-SoTlMQjzWMA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 17",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 17.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1PtWz0Inz8eqrZPDlMFctg?si=Qr3uVeNuSPSgOlIRNeCWMA"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 18",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 18.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1HR5iqDNVtUCJHyAfjVfuW?si=BvDcxpk-RIOAIFygei2gEg"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 19",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 19.pdf",
+        "spotify_link": "https://open.spotify.com/episode/7pxWE0NTDOu5WdLYYfGiJ0?si=Nrm_Cf60TXyXthGEFY3pqw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 20",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 20.pdf",
+        "spotify_link": "https://open.spotify.com/episode/43uanbzTMnAmfTqkNnOp5W?si=sduM5DNARpSSQBAi0OdwEw"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 21",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 21.pdf",
+        "spotify_link": "https://open.spotify.com/episode/3bRhXj0NydFnsnP8gf8jRJ?si=H2ZxKYs3TlKRO21hje_ihQ"
+    },
+    {
+        "name": "NITI Aayog - Strategy for New India @75 - Module 22",
+        "pdf": "NITI Aayog - Strategy for New India @75 - Module 22.pdf",
+        "spotify_link": "https://open.spotify.com/episode/1icqTfl1idPMFGF2eVIOTk?si=gEbBjKIbSCyNuwf7XbLXug"
+    }
+];
